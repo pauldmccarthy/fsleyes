deee4903e3768e90762cdd79dea43329d63166af
Paul McCarthy <pauld.mccarthy@gmail.com>
2016-02-05 10:48:39 +0000

    DiagnosticReportAction report includes settings and saved perspectives.

--- actions/diagnosticreport.py
+++ actions/diagnosticreport.py
@@ -108,6 +108,7 @@ class DiagnosticReportAction(action.Action):
         report['Python']   = '{}  {}'.format(platform.python_version(),
                                              platform.python_compiler())
         report['Version']  = version.__version__
+        report['Settings'] = self.__settingsReport()
         report['Layout']   = perspectives.serialisePerspective(self.__frame)
         report['Overlays'] = overlays
 
@@ -127,6 +128,39 @@ class DiagnosticReportAction(action.Action):
                 ('Display context', self.__displayContextReport(
                     self.__overlayList,
                     vpDisplayCtx))])
+
+        return report
+
+
+    def __settingsReport(self):
+        """Creates and returns a dictionary containing:
+
+         - *FSLeyes* settings stored via the :mod:`.settings` module
+         - Saved perspectives stored via the :mod:`.perspectives` module.
+        """
+
+        import fsl.fsleyes.perspectives as perspectives
+        import fsl.utils.settings       as fslsettings
+        
+        report   = OrderedDict()
+
+        # TODO When you update the fsl.utils.settings module,
+        #      use its functionality to read out *all* saved
+        #      settings, instead of hard coding them here.
+        settings = ['fsldir', 'framelayout', 'frameposition', 'framesize']
+        settings = {s : fslsettings.read(s) for s in settings}
+
+        persps   = perspectives.getAllPerspectives()
+        persps   = {p : fslsettings.read('fsleyes.perspectives.{}'.format(p))
+                    for p in persps}
+
+        for k, v in settings.items():
+            report[k] = str(v)
+
+        report['perspectives'] = ','.join(persps)
+
+        for k, v in persps:
+            report['perspectives.{}'.format(k)] = str(v)
 
         return report
 
