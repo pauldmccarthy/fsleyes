Jesus! Bugfix in ARBPShader - transform matrices were being unravelled in the wrong order. How did this get past my tests? Associcated refactorings to GLVolume code. Unrelated fixes/adjustments to texture_border routine.

fsleyes/gl/gl14/glvolume_funcs.py
@@ -90,9 +90,10 @@ def updateShaderState(self):
     # transformation turns a raw voxel value
     # into a value between 0 and 1, suitable
     # for looking up an appropriate colour
-    # in the 1D colour map texture
+    # in the 1D colour map texture.
     voxValXform = transform.concat(self.colourTexture.getCoordinateTransform(),
                                    self.imageTexture.voxValXform)
+    voxValXform = [voxValXform[0, 0], voxValXform[0, 3], 0, 0]
 
     # The vertex and fragment programs
     # need to know the image shape
@@ -133,16 +134,8 @@ def preDraw(self):
     self.shader.load()
     self.shader.loadAtts()
 
-    opts = self.opts
-
     if isinstance(self, glvolume.GLVolume):
-        if opts.clipImage is None:
-            clipCoordXform = np.eye(4)
-        else:
-            clipCoordXform = transform.concat(
-                self.clipOpts.getTransform('display', 'texture'),
-                opts         .getTransform('texture', 'display'))
-
+        clipCoordXform = self.calculateClipCoordTransform()
         self.shader.setVertParam('clipCoordXform', clipCoordXform)
 
     gl.glEnableClientState(gl.GL_VERTEX_ARRAY)

fsleyes/gl/gl21/glvolume_funcs.py
@@ -127,16 +127,8 @@ def preDraw(self):
     """
     self.shader.load()
 
-    opts = self.opts
-
     if isinstance(self, glvolume.GLVolume):
-        if opts.clipImage is None:
-            clipCoordXform = np.eye(4)
-        else:
-            clipCoordXform = transform.concat(
-                self.clipOpts.getTransform('display', 'texture'),
-                opts         .getTransform('texture', 'display'))
-
+        clipCoordXform = self.calculateClipCoordTransform()
         self.shader.set('clipCoordXform', clipCoordXform)
 
 

fsleyes/gl/shaders/arbp/program.py
@@ -250,10 +250,10 @@ class ARBPShader(object):
         """
 
         pos   = self.vertParamPositions[name]
-        value = np.array(value, dtype=np.float32).ravel('F')
+        value = np.array(value, dtype=np.float32).ravel('C')
         nrows = len(value) // 4
 
-        if nrows == 0:
+        if value.size < 4 or value.size % 4 != 0:
             raise ValueError('Value for {} looks invalid: '
                              '{}'.format(name, value))
 
@@ -276,10 +276,10 @@ class ARBPShader(object):
                   ``True`` if the value was changed, and ``False`` otherwise.
         """
         pos   = self.fragParamPositions[name]
-        value = np.array(value, dtype=np.float32).ravel('F')
+        value = np.array(value, dtype=np.float32).ravel('C')
         nrows = len(value) // 4
 
-        if nrows == 0:
+        if value.size < 4 or value.size % 4 != 0:
             raise ValueError('Value for {} looks invalid: '
                              '{}'.format(name, value))
 