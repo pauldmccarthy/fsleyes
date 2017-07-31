Aspect ratio calculation was wrong

fsleyes/gl/routines.py
@@ -126,19 +126,6 @@ def disabled(capabilities):
         yield
 
 
-def adjust(x, y, w, h):
-    """Adjust the given ``x`` and ``y`` values by the aspect ratio
-    defined by the given ``w`` and ``h`` values.
-    """
-
-    ratio = w / float(h)
-
-    if   ratio > 1: x *= ratio
-    elif ratio < 1: y /= ratio
-
-    return x, y
-
-
 def show2D(xax, yax, width, height, lo, hi, flipx=False, flipy=False):
     """Configures the OpenGL viewport for 2D othorgraphic display.
 
@@ -231,7 +218,7 @@ def lookAt(eye, centre, up):
     return proj
 
 
-def ortho(lo, hi, aspectRatio, zoom):
+def ortho(lo, hi, width, height, zoom):
     """Generates an orthographic projection matrix. The display coordinate
     system origin ``(0, 0, 0)`` is mapped to the centre of the clipping space.
 
@@ -243,7 +230,8 @@ def ortho(lo, hi, aspectRatio, zoom):
 
     :arg lo:          Low ``(x, y, z)`` bounds.
     :arg hi:          High ``(x, y, z)`` bounds.
-    :arg aspectRatio: Viewport aspect ratio (width / height)
+    :arg width:       Canvas width in pixels
+    :arg height:      Canvas height in pixels
     :arg zoom:        Zoom factor. Required to determine suitable near and far
                       clipping plane locations.
     """
@@ -253,17 +241,14 @@ def ortho(lo, hi, aspectRatio, zoom):
 
     xmin, ymin = lo[:2]
     xmax, ymax = hi[:2]
+    zmin, zmax = min(lo), max(hi)
 
-    zmin = min(lo)
-    zmax = max(hi)
+    xmin, xmax, ymin, ymax = preserveAspectRatio(
+        width, height, xmin, xmax, ymin, ymax)
 
     xlen = xmax - xmin
     ylen = ymax - ymin
-    zlen = zmax - zmin
     zlen = np.sqrt(np.sum((hi - lo) ** 2)) * zoom
-
-    if   aspectRatio > 1: xlen *= aspectRatio
-    elif aspectRatio < 1: ylen /= aspectRatio
 
     xhalf = xlen / 2.0
     yhalf = ylen / 2.0
@@ -275,8 +260,7 @@ def ortho(lo, hi, aspectRatio, zoom):
     zmin = -zlen
     zmax = +zlen
 
-    projmat = np.eye(4, dtype=np.float32)
-
+    projmat       = np.eye(4, dtype=np.float32)
     projmat[0, 0] =  2 / (xmax - xmin)
     projmat[1, 1] =  2 / (ymax - ymin)
     projmat[2, 2] = -2 / (zmax - zmin)
@@ -285,6 +269,15 @@ def ortho(lo, hi, aspectRatio, zoom):
     projmat[2, 3] = -(zmax + zmin) / (zmax - zmin)
 
     return projmat
+
+
+def adjust(x, y, w, h):
+    """Adjust the given ``x`` and ``y`` values by the aspect ratio
+    defined by the given ``w`` and ``h`` values.
+    """
+
+    xmin, xmax, ymin, ymax = preserveAspectRatio(w, h, 0, x, 0, y)
+    return (xmax - xmin), (ymax - ymin)
 
 
 def preserveAspectRatio(width, height, xmin, xmax, ymin, ymax, grow=True):

fsleyes/gl/scene3dcanvas.py
@@ -289,28 +289,21 @@ class Scene3DCanvas(props.HasProperties):
         """
 
         width, height = self._getSize()
+        b             = self.__displayCtx.bounds
+        blo           = [b.xlo, b.ylo, b.zlo]
+        bhi           = [b.xhi, b.yhi, b.zhi]
+        zoom          = self.zoom / 100.0
 
-        if width == 0 or height == 0:
-            return False
+        if width == 0 or height == 0:    return False
+        if np.any(np.isclose(blo, bhi)): return False
 
-        b   = self.__displayCtx.bounds
-        blo = [b.xlo, b.ylo, b.zlo]
-        bhi = [b.xhi, b.yhi, b.zhi]
-
-        if np.any(np.isclose(blo, bhi)):
-            return False
-
-        # Generate the view atrix
+        # Generate the view and projection matrices
         self.__genViewMatrix()
-
-        aratio = width / float(height)
-        zoom   = self.zoom / 100.0
-        self.__projMat = glroutines.ortho(blo, bhi, aratio, zoom)
+        self.__projMat = glroutines.ortho(blo, bhi, width, height, zoom)
 
         gl.glViewport(0, 0, width, height)
         gl.glMatrixMode(gl.GL_PROJECTION)
         gl.glLoadMatrixf(self.__projMat.ravel('F'))
-
         gl.glMatrixMode(gl.GL_MODELVIEW)
         gl.glLoadIdentity()
 