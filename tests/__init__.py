c80b85b2895f5eb3a64e4804801868de473e1ddb
Paul McCarthy <pauldmccarthy@gmail.com>
2017-11-14 15:06:28 +1030

    Unit tests for screenshot action. Not quite ready to be used yet.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,3 +4,210 @@
 #
 # Author: Paul McCarthy <pauldmccarthy@gmail.com>
 #
+
+import          os
+import          gc
+import          time
+import          shutil
+import          tempfile
+import          traceback
+import          contextlib
+
+import          wx
+import numpy as np
+
+import fsleyes_props                as props
+import                                 fsleyes
+import fsleyes.frame                as fslframe
+import fsleyes.main                 as fslmain
+import fsleyes.actions.frameactions as frameactions  # noqa
+import fsleyes.gl                   as fslgl
+import fsleyes.colourmaps           as colourmaps
+import fsleyes.displaycontext       as dc
+import fsleyes.overlay              as fsloverlay
+
+
+# Under GTK, a single call to
+# yield just doesn't cut it
+def realYield(centis=10):
+    for i in range(int(centis)):
+        wx.YieldIfNeeded()
+        time.sleep(0.01)
+
+
+@contextlib.contextmanager
+def tempdir():
+    """Returnsa context manager which creates and returns a temporary
+    directory, and then deletes it on exit.
+    """
+
+    testdir = tempfile.mkdtemp()
+    prevdir = os.getcwd()
+    try:
+
+        os.chdir(testdir)
+        yield testdir
+
+    finally:
+        os.chdir(prevdir)
+        shutil.rmtree(testdir)
+
+
+def run_with_fsleyes(func, *args, **kwargs):
+    """Create a ``FSLeyesFrame`` and run the given function. """
+
+    gc.collect()
+
+    propagateRaise = kwargs.pop('propagateRaise', True)
+    startingDelay  = kwargs.pop('startingDelay',  500)
+    finishingDelay = kwargs.pop('finishingDelay', 500)
+    callAfterApp   = kwargs.pop('callAfterApp',   None)
+
+    result = [None]
+    raised = [None]
+    app    = [None]
+
+    def dorun():
+
+        fsleyes.initialise()
+        props.initGUI()
+        colourmaps.init()
+
+        overlayList = fsloverlay.OverlayList()
+        displayCtx  = dc.DisplayContext(overlayList)
+        frame       = fslframe.FSLeyesFrame(None,
+                                            overlayList,
+                                            displayCtx)
+
+        app[0].SetOverlayListAndDisplayContext(overlayList, displayCtx)
+        app[0].SetTopWindow(frame)
+
+        frame.Show()
+
+        try:
+            if func is not None:
+                result[0] = func(frame,
+                                 overlayList,
+                                 displayCtx,
+                                 *args,
+                                 **kwargs)
+
+        except Exception as e:
+            traceback.print_exc()
+            raised[0] = e
+
+        finally:
+            def finish():
+                frame.Close(askUnsaved=False, askLayout=False)
+                app[0].ExitMainLoop()
+            wx.CallLater(finishingDelay, finish)
+
+    def glcallback():
+        fslgl.bootstrap((2, 1))
+        wx.CallLater(startingDelay, dorun)
+
+    app[0] = fslmain.FSLeyesApp()
+
+    if callAfterApp is not None:
+        callAfterApp()
+
+    dummy = wx.Frame(None)
+    dummy.Show()
+
+    wx.CallAfter(fslgl.getGLContext, parent=dummy, ready=glcallback)
+    app[0].MainLoop()
+
+    time.sleep(1)
+
+    if raised[0] and propagateRaise:
+        raise raised[0]
+
+    del app[0]
+
+    return result[0]
+
+
+def run_with_viewpanel(func, vptype, *args, **kwargs):
+    def inner(frame, overlayList, displayCtx, *a, **kwa):
+        panel = frame.addViewPanel(vptype)
+        return func(panel, overlayList, displayCtx, *a, **kwa)
+    return run_with_fsleyes(inner, *args, **kwargs)
+
+
+def run_with_orthopanel(func, *args, **kwargs):
+    """Create a ``FSLeyesFrame`` with an ``OrthoPanel`` and run the given
+    function.
+    """
+    from fsleyes.views.orthopanel import OrthoPanel
+    return run_with_viewpanel(func, OrthoPanel, *args, **kwargs)
+
+
+def run_with_lightboxpanel(func, *args, **kwargs):
+    """Create a ``FSLeyesFrame`` with a ``LightBoxPanel`` and run the given
+    function.
+    """
+    from fsleyes.views.lightboxpanel import LightBoxPanel
+    return run_with_viewpanel(func, LightBoxPanel, *args, **kwargs)
+
+
+def run_with_scene3dpanel(func, *args, **kwargs):
+    """Create a ``FSLeyesFrame`` with a ``Scene3DPanel`` and run the given
+    function.
+    """
+    from fsleyes.views.scene3dpanel import Scene3DPanel
+    return run_with_viewpanel(func, Scene3DPanel, *args, **kwargs)
+
+
+def run_with_timeseriespanel(func, *args, **kwargs):
+    """Create a ``FSLeyesFrame`` with a ``TimeSeriesPanel`` and run the given
+    function.
+    """
+    from fsleyes.views.timeseriespanel import TimeSeriesPanel
+    return run_with_viewpanel(func, TimeSeriesPanel, *args, **kwargs)
+
+
+def run_with_histogrampanel(func, *args, **kwargs):
+    """Create a ``FSLeyesFrame`` with a ``HistogramPanel`` and run the given
+    function.
+    """
+    from fsleyes.views.histogrampanel import HistogramPanel
+    return run_with_viewpanel(func, HistogramPanel, *args, **kwargs)
+
+
+def run_with_powerspectrumpanel(func, *args, **kwargs):
+    """Create a ``FSLeyesFrame`` with a ``PowerSpectrumPanel`` and run the
+    given function.
+    """
+    from fsleyes.views.powerspectrumpanel import PowerSpectrumPanel
+    return run_with_viewpanel(func, PowerSpectrumPanel, *args, **kwargs)
+
+
+def compare_images(img1, img2, threshold):
+    """Compares two images using the euclidean distance in RGB space
+    between pixels. Returns a tuple containing:
+
+     - A boolean value indicating whether the test passed (the images
+       were the same).
+
+     - The sum of the normalised RGB distance between all pixels.
+    """
+
+    # Discard alpha values
+    img1 = img1[:, :, :3]
+    img2 = img2[:, :, :3]
+
+    if img1.shape != img2.shape:
+        return False, 0
+
+    flat1   = img1.reshape(-1, 3)
+    flat2   = img2.reshape(-1, 3)
+
+    dist    = np.sqrt(np.sum((flat1 - flat2) ** 2, axis=1))
+    dist    = dist.reshape(img1.shape[:2])
+    dist    = dist / np.sqrt(3 * 255 * 255)
+
+    ttlDiff = np.sum(dist)
+
+    passed = ttlDiff <= threshold
+
+    return passed, ttlDiff
