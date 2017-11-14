f45d800df5b70a1e21fc0aca6f3bd6a47c1601bb
Paul McCarthy <pauldmccarthy@gmail.com>
2017-11-14 15:06:28 +1030

    Unit tests for screenshot action. Not quite ready to be used yet.

---
+++ tests/test_screenshot.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+#
+# test_screenshot.py - Test fsleyes.actions.screenshot
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+
+import os.path          as op
+
+import fsl.data.image   as fslimage
+import fsl.utils.idle   as idle
+
+from . import (run_with_orthopanel,
+               run_with_lightboxpanel,
+               run_with_scene3dpanel,
+               run_with_timeseriespanel,
+               run_with_histogrampanel,
+               run_with_powerspectrumpanel,
+               tempdir,
+               realYield,
+               compare_images)
+
+
+datadir = op.join(op.dirname(__file__), 'testdata')
+
+
+def _test_screenshot(panel, overlayList, displayCtx, stype, imgfile):
+
+    import matplotlib.image           as mplimg
+    import fsleyes.actions.screenshot as screenshot
+
+    img = fslimage.Image(op.join(datadir, imgfile))
+
+    overlayList.append(img)
+
+    with tempdir():
+
+        realYield()
+        idle.idle(screenshot.screenshot,
+                  panel,
+                  'screenshot_{}.png'.format(stype))
+        realYield()
+
+        benchmark  = op.join(datadir, 'screenshot_{}.png'.format(stype))
+        screenshot = mplimg.imread('screenshot.png')
+        benchmark  = mplimg.imread(benchmark)
+
+        return compare_images(screenshot, benchmark, 50)[0]
+
+def test_screenshot_ortho():
+    run_with_orthopanel(_test_screenshot, 'ortho', 'MNI152_T1_2mm_brain')
+
+def test_screenshot_lightbox():
+    run_with_lightboxpanel(_test_screenshot, 'lightbox', 'MNI152_T1_2mm_brain')
+
+def test_screenshot_3d():
+    run_with_scene3dpanel(_test_screenshot, '3d', 'MNI152_T1_2mm_brain')
+
+def test_screenshot_timeseries():
+    run_with_timeseriespanel(_test_screenshot, 'timeseries', 'av')
+
+def test_screenshot_histogram():
+    run_with_histogrampanel(_test_screenshot, 'histogram', 'av')
+
+def test_screenshot_powerspectrum():
+    run_with_powerspectrumpanel(_test_screenshot, 'powerspectrum', 'av')
