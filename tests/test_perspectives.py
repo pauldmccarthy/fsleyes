d35807bad747d8eb382edce1c2297fb53d6f5c61
Paul McCarthy <pauldmccarthy@gmail.com>
2017-12-04 16:00:15 +1030

    More super basic tests

---
+++ tests/test_perspectives.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#
+# test_perspectives.py -
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+
+
+import os.path as op
+
+import fsl.data.image as fslimage
+
+import fsleyes.perspectives as perspectives
+
+from . import run_with_fsleyes, realYield
+
+
+datadir = op.join(op.dirname(__file__), 'testdata')
+
+
+def _test_perspective(frame, overlayList, displayCtx, persp):
+    img = fslimage.Image(op.join(datadir, 'av'))
+    overlayList.append(img)
+    perspectives.loadPerspective(frame, persp)
+    realYield(100)
+
+
+def test_default():
+    run_with_fsleyes(_test_perspective, 'default')
+
+
+def test_ortho():
+    run_with_fsleyes(_test_perspective, 'ortho')
+
+
+def test_lightbox():
+    run_with_fsleyes(_test_perspective, 'lightbox')
+
+
+def test_3d():
+    run_with_fsleyes(_test_perspective, '3d')
+
+
+def test_melodic():
+    run_with_fsleyes(_test_perspective, 'melodic')
+
+
+def test_feat():
+    run_with_fsleyes(_test_perspective, 'feat')
