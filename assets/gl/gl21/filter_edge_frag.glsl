afbfb2879307bdf2038a0acf89f8156f1c861180
Paul McCarthy <pauldmccarthy@gmail.com>
2018-01-07 12:24:38 +0000

    Added same logic to gl21 filter

--- assets/gl/gl21/filter_edge_frag.glsl
+++ assets/gl/gl21/filter_edge_frag.glsl
@@ -9,17 +9,19 @@
 
 uniform sampler2D texture;
 uniform vec2      offsets;
+uniform bool      outline;
 varying vec2      fragTexCoord;
 
 void main(void) {
 
   vec4 colour = texture2D(texture, fragTexCoord);
   vec4 tol    = 1.0 / vec4(255, 255, 255, 255);
+  bool isEdge = edge2D(texture, fragTexCoord, colour, tol, offsets);
 
   /*
    * If the fragment lies on an edge
    * colour it, otherwise clear it.
    */
-  if (edge2D(texture, fragTexCoord, colour, tol, offsets)) gl_FragColor = colour;
-  else                                                     discard;
+  if (isEdge == outline) gl_FragColor = colour;
+  else                   discard;
 }
