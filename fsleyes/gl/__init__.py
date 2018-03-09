dedb55dfe132563e00ac485d1dc85b2e26210957
Paul McCarthy <pauldmccarthy@gmail.com>
2018-03-09 14:22:06 +0000

    try to check for freeglut, rather than testing for linux vs mac

--- fsleyes/gl/__init__.py
+++ fsleyes/gl/__init__.py
@@ -365,8 +365,10 @@ def bootstrap(glVersion=None):
     log.debug('Using OpenGL {} implementation with renderer {}'.format(
         verstr, renderer))
 
-    # If on linux, we need to call glutInit.
-    # If on OSX, we don't need to bother.
+    # If using freeglut, we need to call
+    # glutInit. If not (e.g. on mac, which
+    # provides its own GLUT implementation),
+    # we don't need to bother.
     #
     # note: GLUT is only required for text
     #       rendering. The GLUT requirement
@@ -377,8 +379,11 @@ def bootstrap(glVersion=None):
     #       as a requirement. Off-screen
     #       rendering is possible via other
     #       means (e.g. xvfb-run).
-    if fslplatform.os == 'Linux':
-        import OpenGL.GLUT as GLUT
+
+    import OpenGL.GLUT as GLUT
+    # it would be nice if there were a
+    # nicer way to test for freeglut
+    if bool(GLUT.fgDeinitialize):
         GLUT.glutInit()
 
     # Populate this module, and set
