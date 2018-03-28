e47ab9498873fe91808801408f3aef94460ab945
Paul McCarthy <pauldmccarthy@gmail.com>
2018-03-28 16:32:33 +0100

    Fixed two bugs in SH rendering
     - FODs would occasionally look like squashed butterflies instead of regular
       butterfiles due to incorrect array reshaping in numpy.
    
     - Voxels array was not being adjusted after FOV/radius thresholding

--- fsleyes/gl/glsh.py
+++ fsleyes/gl/glsh.py
@@ -112,7 +112,7 @@ class GLSH(glvector.GLVectorBase):
         # This texture gets updated on
         # draw calls, so we want it to
         # run on the main thread.
-        self.radTexture = textures.Texture3D('{}_radTexture'.format('fuck'),
+        self.radTexture = textures.Texture3D('{}_radTexture'.format(id(self)),
                                              threaded=False)
 
         # Usin preinit here (see GLVectorBase)
@@ -350,10 +350,8 @@ class GLSH(glvector.GLVectorBase):
                   (x >= shape[0]) | \
                   (y >= shape[1]) | \
                   (z >= shape[2])
-
-        x = np.array(x[~out], dtype=np.int32)
-        y = np.array(y[~out], dtype=np.int32)
-        z = np.array(z[~out], dtype=np.int32)
+        voxels  = np.asarray(voxels[~out, :], dtype=np.uint32)
+        x, y, z = voxels.T
 
         # The dot product of the SH parameters with
         # the SH coefficients for a single voxel gives
@@ -365,13 +363,13 @@ class GLSH(glvector.GLVectorBase):
         params = self.__shParams
         vols   = self.__coefVolumeMask()
         coefs  = self.image.nibImage.get_data()[x, y, z, vols]
-        radii  = np.dot(params, coefs.T)
+        radii  = np.dot(coefs, params.T)
 
         # Remove sub-threshold voxels/radii
         if opts.radiusThreshold > 0:
-            aboveThres = np.any(radii >= opts.radiusThreshold, axis=0)
-            radii      = np.array(radii[:, aboveThres])
-            voxels     = np.array(voxels[aboveThres, :])
+            aboveThres = np.any(radii >= opts.radiusThreshold, axis=1)
+            radii      = radii[ aboveThres, :]
+            voxels     = voxels[aboveThres, :]
 
         # No voxels - nothing to do
         if voxels.shape[0] == 0:
@@ -390,7 +388,7 @@ class GLSH(glvector.GLVectorBase):
         # So here we are calculating a suitable
         # 3D shape in which the radius values can
         # be stored. The shape may end up being
-        # larger than necessary, if the number of
+
         # voxels cannot easily be divided/dispersed
         # across the other dimensions.
         radTexShape = np.array([radii.size, 1, 1])
@@ -422,12 +420,26 @@ class GLSH(glvector.GLVectorBase):
             radTexShape[imax] /= divisor
             radTexShape[imin] *= divisor
 
-        # Resize and reshape the
-        # radius array as needed
+        # Resize and reshape the radius array as needed.
+        # Radii starts off as a (voxels, vertices) array.
+        # We flatten it to 1D (by voxels, i.e. with
+        # vertices the fastest changing)
+        radii      = radii.reshape(-1)
         radTexSize = np.prod(radTexShape)
-        if radTexSize != radii.size:
-            radii.resize(radTexSize)
 
+        # if the texture size needs to be bigger,
+        # create a new array, and copy the radii
+        # data into it
+        if radTexSize != radii.size:
+            tmp = np.zeros(radTexSize, dtype=radii.dtype)
+            tmp[:radii.size] = radii
+            radii = tmp
+
+        # reshape to the texture size, making
+        # sure that the first dimension is the
+        # fastest changing (as this is what
+        # OpenGL requires). This ensures that
+        # the data remains contiguous in memory
         radii = radii.reshape(radTexShape, order='F')
 
         # Copy the data to the texture
