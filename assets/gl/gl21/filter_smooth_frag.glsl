6b1339ffa6e66414e6180def24e08137c16fe8f3
Paul McCarthy <pauldmccarthy@gmail.com>
2018-04-24 11:39:23 +0100

    Made the smoothing filter actually do smoothing.

--- assets/gl/gl21/filter_smooth_frag.glsl
+++ assets/gl/gl21/filter_smooth_frag.glsl
@@ -1,33 +1,80 @@
 /*
  * Filter fragment shader which performs basic smoothing on a texture.
+ *
+ * Each fragment is replaced with a weighted average of the fragment and its
+ * neighbours, with the weight for each neighbouring fragment linearly
+ * proportional to its distance from the target fragment.
  */
 #version 120
 
-uniform sampler2D texture;
-uniform vec2      offset;
 
-varying vec2      texCoord;
+/* Texture to be smoothed. */
+uniform sampler2D texture;
+
+/* Distance between adjacent fragments, in texture coordinates. */
+uniform vec2 offsets;
+
+/*
+ * nSize of the smoothing kernel - a square kernel of size (kernSize, kernSize)
+ * will be used. If kernSize is even, it will be forced to be odd.
+ */
+uniform int kernSize;
+
+/* Coordinates of current fragment. */
+varying vec2 fragTexCoord;
+
 
 void main(void) {
 
-  float x    = texCoord.x;
-  float y    = texCoord.y;
-  float xoff = offset.x;
-  float yoff = offset.y;
+  float dist;
+  vec2  point;
+  int   nsteps;
+  int   midstep;
 
-  vec4 rgba  = texture2D(texture, vec2(x,        y));
-  vec4 left  = texture2D(texture, vec2(x - xoff, y));
-  vec4 right = texture2D(texture, vec2(x + xoff, y));
-  vec4 above = texture2D(texture, vec2(x,        y + yoff));
-  vec4 below = texture2D(texture, vec2(x,        y - yoff));
+  vec2  off;
+  vec4  val;
+  float maxdist;
+  float cumdist = 0;
+  vec4  rgba    = vec4(0);
 
-  if (left.a  == 0) left  = rgba;
-  if (right.a == 0) right = rgba;
-  if (above.a == 0) above = rgba;
-  if (below.a == 0) below = rgba;
+  // force kernel size to be odd
+  if (mod(kernSize, 2) == 0)
+    nsteps = kernSize + 1;
+  else
+    nsteps = kernSize;
 
-  rgba.rgb += 0.5 * (left.rgb + right.rgb + above.rgb + below.rgb);
-  rgba.rgb /= 3;
+  midstep = nsteps / 2;
 
-  gl_FragColor = rgba;
+  // distance from the target fragment
+  // to the farthest fragments (the
+  // corners of the kernel). Used for
+  // normalisation.
+  if (midstep == 0)
+    maxdist = 1;
+  else
+    maxdist = distance(vec2(0, 0), vec2(midstep, midstep));
+
+  for (int xi = 0; xi < nsteps; xi++) {
+    for (int yi = 0; yi < nsteps; yi++) {
+
+      off   = vec2(xi - midstep, yi - midstep);
+      point = fragTexCoord + off * offsets;
+      val   = texture2D(texture, point);
+
+      if (val.a == 0) {
+        continue;
+      }
+
+      // Invert the distance so closer fragments
+      // weigh more. Accumulate the total distance
+      // so we can normalise at the end (with the
+      // effect that all of the kernel weights will
+      // add to 1).
+      dist     = maxdist - distance(vec2(0, 0), off);
+      cumdist += dist;
+      rgba    += val * dist;
+    }
+  }
+
+  gl_FragColor = rgba / cumdist;
 }
