9a92dc31b2a11bba6ac4efc03a06039fb45dc39d
Paul McCarthy <pauldmccarthy@gmail.com>
2018-05-10 16:00:52 +0100

    CI: Ugh. Retrieving exit codes of test batches.

--- .ci/test_template.sh
+++ .ci/test_template.sh
@@ -34,14 +34,22 @@ if [ "$TEST_STYLE"x != "x" ]; then pylint --output-format=colorized fsleyes || t
 if [ "$TEST_STYLE"x != "x" ]; then exit 0; fi
 
 # Run the tests  - test overlay types for GL14 as well
-FSLEYES_TEST_GL=2.1 MPLBACKEND='wxagg' xvfb-run -s "-screen 0 640x480x24" pytest --cov-report= --cov-append -m "not overlaytest" || true
-failed=$?
+export MPLBACKEND=wxagg
+export FSLEYES_TEST_GL=2.1
+((xvfb-run -s "-screen 0 640x480x24" pytest --cov-report= --cov-append -m "not overlaytest" && echo "0" > status) || echo "1" > status) || true
+status=`cat status`
+failed=$status
 sleep 5
-FSLEYES_TEST_GL=2.1 MPLBACKEND='wxagg' xvfb-run -s "-screen 0 640x480x24" pytest --cov-report= --cov-append -m "overlaytest" || true
-failed=`echo "$? + $failed" | bc`
+
+((xvfb-run -s "-screen 0 640x480x24" pytest --cov-report= --cov-append -m "overlaytest" && echo "0" > status) || echo "1" > status) || true
+status=`cat status`
+failed=`echo "$status + $failed" | bc`
 sleep 5
-FSLEYES_TEST_GL=1.4 MPLBACKEND='wxagg' xvfb-run -s "-screen 0 640x480x24" pytest --cov-report= --cov-append -m "overlaytest" || true
-failed=`echo "$? + $failed" | bc`
+
+export FSLEYES_TEST_GL=1.4
+((xvfb-run -s "-screen 0 640x480x24" pytest --cov-report= --cov-append -m "overlaytest" && echo "0" > status) || echo "1" > status) || true
+status=`cat status`
+failed=`echo "$status + $failed" | bc`
 
 python -m coverage report
 
