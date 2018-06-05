7e3dafe0a07fc3656a563ed7c2da70cd1ececd7b
Paul McCarthy <pauldmccarthy@gmail.com>
2018-06-05 15:19:03 +0100

    CI: Ugh, errors in build_pypi_dist.sh changes

--- .ci/build_pypi_dist.sh
+++ .ci/build_pypi_dist.sh
@@ -2,18 +2,14 @@
 
 set -e
 
+source /test.venv/bin/activate
+
 pip install wheel
 python setup.py sdist
 python setup.py bdist_wheel
 
+pip install dist/*.whl
+pip uninstall -y fsleyes
 
-python3.5 -m venv pypi_test.venv
-source activate pypi_test.venv
-pip install dist/*whl
-deactivate
-
-rm -r pypi_test.venv
-python3.5 -m venv pypi_test.venv
-source activate pypi_test.venv
-pip install dist/*tar.gz
-deactivate
+pip install dist/*.tar.gz
+pip uninstall -y fsleyes
