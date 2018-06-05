a8f0dc551b35b620cd3253b8d5f07079c739cafe
Paul McCarthy <pauldmccarthy@gmail.com>
2018-06-05 13:31:07 +0100

    CI: Test pypi package installation

--- .ci/build_pypi_dist.sh
+++ .ci/build_pypi_dist.sh
@@ -5,3 +5,15 @@ set -e
 pip install wheel
 python setup.py sdist
 python setup.py bdist_wheel
+
+
+python3.5 -m venv test.venv
+source activate test.venv
+pip install dist/*whl
+deactivate
+
+rm -r test.venv
+python3.5 -m venv test.venv
+source activate test.venv
+pip install dist/*tar.gz
+deactivate
