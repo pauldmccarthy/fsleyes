a142e7b56d0b9825e0615219d5d6c7f2d270dc7f
Paul McCarthy <pauldmccarthy@gmail.com>
2019-07-26 15:00:52 +0100

    RF/ENH: SelectionTexture refactored so both 2D and 3D textures can be supported

--- fsleyes/gl/textures/selectiontexture.py
+++ fsleyes/gl/textures/selectiontexture.py
@@ -4,156 +4,135 @@
 #
 # Author: Paul McCarthy <pauldmccarthy@gmail.com>
 #
-"""This module provides the :class:`SelectionTexture` class, a
-:class:`.Texture` type which can be used to store :class:`.Selection`
-instances.
+"""This module provides the :class:`SelectionTexture2D` and
+:class:`SelectionTexture3D` classes, :class:`.Texture` types which can be used
+to store :class:`.Selection` instances.
 
-The :class:`SelectionTexture` class is used by the :class:`.VoxelSelection`
-annotation.
+The ``SelectionTexture2D/3D`` classes are used by the :class:`.VoxelSelection`
+annotation to display the contents of a :class:`.Selection` instance.
 """
+
 
 import logging
 
 import numpy     as np
 import OpenGL.GL as gl
 
-from . import       texture
+from . import       texture2d
+from . import       texture3d
 
 
 log = logging.getLogger(__name__)
 
 
-class SelectionTexture(texture.Texture):
-    """The ``SelectionTexture`` class is a :class:`.Texture` which can be used
-    to represent a :class:`.Selection` instance.  The ``Selection`` image array
-    is stored as a single channel 3D texture, which is updated whenever the
-    :attr:`.Selection.selection` property changes, and whenever the
-    :meth:`refresh` method is called.
+class SelectionTextureBase(object):
+    """Base class shared by the :class:`SelectionTexture2D` and
+    :class:`SelectionTexture3D`. Manages updates from the :class:`.Selection`
+    object.
     """
 
-
-    def __init__(self, name, selection):
-        """Create a ``SelectionTexture``.
-
-        :arg name:      A unique name for this ``SelectionTexture``.
-
-        :arg selection: The :class:`.Selection` instance.
+    def __init__(self, selection):
         """
-
-        texture.Texture.__init__(self, name, 3, 1)
-
+        This method must be called *after* :meth:`.Texture.__init__`.
+        """
         self.__selection = selection
-
         selection.register(self.name, self.__selectionChanged)
-
-        self.__init()
-        self.refresh()
+        self.__selectionChanged()
 
 
-    def __init(self):
-        """Called by :meth:`__init__`. Configures the GL texture. """
-
-        self.bindTexture()
-
-        gl.glTexParameteri(gl.GL_TEXTURE_3D,
-                           gl.GL_TEXTURE_MAG_FILTER,
-                           gl.GL_NEAREST)
-        gl.glTexParameteri(gl.GL_TEXTURE_3D,
-                           gl.GL_TEXTURE_MIN_FILTER,
-                           gl.GL_NEAREST)
-
-        gl.glTexParameterfv(gl.GL_TEXTURE_3D,
-                            gl.GL_TEXTURE_BORDER_COLOR,
-                            np.array([0, 0, 0, 0], dtype=np.float32))
-
-        gl.glTexParameteri(gl.GL_TEXTURE_3D,
-                           gl.GL_TEXTURE_WRAP_S,
-                           gl.GL_CLAMP_TO_BORDER)
-        gl.glTexParameteri(gl.GL_TEXTURE_3D,
-                           gl.GL_TEXTURE_WRAP_T,
-                           gl.GL_CLAMP_TO_BORDER)
-        gl.glTexParameteri(gl.GL_TEXTURE_3D,
-                           gl.GL_TEXTURE_WRAP_R,
-                           gl.GL_CLAMP_TO_BORDER)
-
-        shape = self.__selection.getSelection().shape
-        gl.glTexImage3D(gl.GL_TEXTURE_3D,
-                        0,
-                        gl.GL_ALPHA8,
-                        shape[0],
-                        shape[1],
-                        shape[2],
-                        0,
-                        gl.GL_ALPHA,
-                        gl.GL_UNSIGNED_BYTE,
-                        None)
-
-        self.unbindTexture()
+    @property
+    def selection(self):
+        """Returns a reference to the :class:`.Selection` object. """
+        return self.__selection
 
 
     def destroy(self):
-        """Must be called when this ``SelectionTexture`` is no longer needed.
-        Calls the :meth:`.Texture.destroy` method, and removes the listener
-        on the :attr:`.Selection.selection` property.
+        """Must be called when this ``SelectionTextureBase`` is no longer
+        needed. Removes the listener on the :attr:`.Selection.selection`
+        property.
         """
-        texture.Texture.destroy(self)
         self.__selection.deregister(self.name)
         self.__selection = None
 
 
-    def refresh(self, block=None, offset=None):
-        """Refreshes the texture data from the :class:`.Selection` image
-        data.
-
-        If ``block`` and ``offset`` are not provided, the entire texture is
-        refreshed from the :class:`.Selection` instance. If you know that only
-        part of the selection data has changed, you can use the ``block`` and
-        ``offset`` arguments to refresh a specific region of the texture
-        (which will be faster than a full : refresh).
-
-        :arg block:  A 3D ``numpy`` array containing the new selection data.
-
-        :arg offset: A tuple specifying the ``(x, y, z)`` offset of the
-                     ``block`` into the selection array.
-        """
-
-        if block is None or offset is None:
-            data   = self.__selection.getSelection()
-            offset = [0, 0, 0]
-        else:
-            data = block
-
-        data = data * 255
-
-        log.debug('Updating selection texture (offset {}, size {})'.format(
-            offset, data.shape))
-
-        self.bindTexture()
-        gl.glTexSubImage3D(gl.GL_TEXTURE_3D,
-                           0,
-                           offset[0],
-                           offset[1],
-                           offset[2],
-                           data.shape[0],
-                           data.shape[1],
-                           data.shape[2],
-                           gl.GL_ALPHA,
-                           gl.GL_UNSIGNED_BYTE,
-                           data.ravel('F'))
-        self.unbindTexture()
-
-
     def __selectionChanged(self, *a):
         """Called when the :attr:`.Selection.selection` changes. Updates
-        the texture data via the :meth:`refresh` method.
+        the texture data via the :meth:`.Texture.doPatch` method.
         """
 
         old, new, offset = self.__selection.getLastChange()
 
         if new is None:
-            data   = self.__selection.getSelection()
-            offset = [0, 0, 0]
-        else:
-            data = new
+            data = self.__selection.getSelection()
+            data = (data * 255).astype(np.uint8)
+            self.set(data=data)
 
-        self.refresh(data, offset)
+        else:
+            data = (new * 255).astype(np.uint8)
+            self.doPatch(data, offset)
+
+
+class SelectionTexture3D(texture3d.Texture3D, SelectionTextureBase):
+    """The ``SelectionTexture3D`` class is a :class:`.Texture3D` which can be
+    used to store a :class:`.Selection` instance.  The ``Selection`` image
+    array is stored as a single channel 3D texture, which is updated whenever
+    the :attr:`.Selection.selection` property changes - updates are managed by
+    the :class:`SelectionTextureBase` class.
+    """
+
+
+    def __init__(self, name, selection):
+        """Create a ``SelectionTexture3D``.
+
+        :arg name:      A unique name for this ``SelectionTexture3D``.
+        :arg selection: The :class:`.Selection` instance.
+        """
+
+        texture3d.Texture3D .__init__(self,
+                                      name,
+                                      nvals=1,
+                                      textureFormat=gl.GL_ALPHA,
+                                      internalFormat=gl.GL_ALPHA8)
+        SelectionTextureBase.__init__(self, selection)
+
+
+    def destroy(self):
+        """Must be called when this ``SelectionTexture3D`` is no longer needed.
+        Calls the :meth:`.Texture.destroy` method, and removes the listener
+        on the :attr:`.Selection.selection` property.
+        """
+        texture3d.Texture3D .destroy(self)
+        SelectionTextureBase.destroy(self)
+
+
+class SelectionTexture2D(texture2d.Texture2D, SelectionTextureBase):
+    """The ``SelectionTexture2D`` class is a :class:`.Texture2D` which can be
+    used to store a :class:`.Selection` instance.  The ``Selection`` image
+    array is stored as a single channel 2D texture, which is updated whenever
+    the :attr:`.Selection.selection` property changes - updates are managed by
+    the :class:`SelectionTextureBase` class..
+    """
+
+
+    def __init__(self, name, selection):
+        """Create a ``SelectionTexture2D``.
+
+        :arg name:      A unique name for this ``SelectionTexture2D``.
+        :arg selection: The :class:`.Selection` instance.
+        """
+
+        texture2d.Texture2D .__init__(self,
+                                      name,
+                                      nvals=1,
+                                      textureFormat=gl.GL_ALPHA,
+                                      internalFormat=gl.GL_ALPHA8)
+        SelectionTextureBase.__init__(self, selection)
+
+
+    def destroy(self):
+        """Must be called when this ``SelectionTexture2D`` is no longer needed.
+        Calls the :meth:`.Texture.destroy` method, and removes the listener
+        on the :attr:`.Selection.selection` property.
+        """
+        texture2d.Texture2D .destroy(self)
+        SelectionTextureBase.destroy(self)
