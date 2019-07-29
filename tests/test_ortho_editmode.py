b258b2528d999ee688edd302b48339d2fcac98c3
Paul McCarthy <pauldmccarthy@gmail.com>
2019-07-29 11:32:32 +0100

    TEST: Test ortho edit with 2d images

--- tests/test_ortho_editmode.py
+++ tests/test_ortho_editmode.py
@@ -6,23 +6,31 @@
 #
 
 import os.path as op
+import functools as ft
 
 import numpy as np
 
 import wx
 
+import fsl.utils.transform as transform
 from fsl.data.image import Image
 
-from . import run_with_orthopanel, realYield, simclick
+from . import run_with_orthopanel, realYield
 
 
 datadir = op.join(op.dirname(__file__), 'testdata')
 
 
-def _test_fillSelection(ortho, overlayList, displayCtx):
-    sim = wx.UIActionSimulator()
-    img = Image(op.join(datadir, '3d'))
+def _test_fillSelection(ortho, overlayList, displayCtx, img, canvas=None):
+
+    if canvas is None: cidx = 2
+    else:              cidx = canvas
+
     overlayList.append(img)
+    realYield()
+    displayCtx.displaySpace  = img
+    displayCtx.worldLocation = transform.transform([0, 0, 0], img.
+                                                   voxToWorldMat)
     realYield()
     ortho.profile = 'edit'
     realYield(20)
@@ -33,21 +41,29 @@ def _test_fillSelection(ortho, overlayList, displayCtx):
     profile.selectionSize = 1
     profile.fillValue     = 999
 
-    xcanvas = ortho.getXCanvas()
-    opts    = ortho.displayCtx.getOpts(img)
+    canvas = ortho.getGLCanvases()[cidx]
+    opts   = ortho.displayCtx.getOpts(img)
 
     realYield(20)
 
-    pxs = np.random.randint(0, img.shape[1], 10)
-    pys = np.random.randint(0, img.shape[2], 10)
+    if   cidx == 0: xidx, yidx = 1, 2
+    elif cidx == 1: xidx, yidx = 0, 2
+    elif cidx == 2: xidx, yidx = 0, 1
+
+    pxs = np.random.randint(0, img.shape[xidx], 10)
+    pys = np.random.randint(0, img.shape[yidx], 10)
 
     for i in range(10):
-        px, py = pxs[i] + 0.5, pys[i] + 0.5
-        pos    = opts.transformCoords((8, px, py), 'voxel', 'display')
+        px, py   = pxs[i], pys[i]
+        pt       = [None, None, None]
+        pt[xidx] = px
+        pt[yidx] = py
+        pt[cidx] = 0
+        pos      = opts.transformCoords(pt, 'voxel', 'display')
 
-        px, py = pos[1], pos[2]
+        px, py = pos[xidx], pos[yidx]
 
-        bounds = xcanvas.opts.displayBounds
+        bounds = canvas.opts.displayBounds
 
         xoff = bounds.xlo - opts.bounds.ylo
         yoff = bounds.ylo - opts.bounds.zlo
@@ -55,20 +71,22 @@ def _test_fillSelection(ortho, overlayList, displayCtx):
         px = (px - xoff) / bounds.xlen
         py = (py - yoff) / bounds.ylen
 
-        simclick(sim, xcanvas, pos=(px, 1 - py))
-        realYield(10)
+        profile._selModeLeftMouseDown(None, canvas, (1 - px, py), pos)
+        realYield(5)
+        profile._selModeLeftMouseUp(None, canvas, (1 - px, py), pos)
+        realYield(5)
 
     sel = np.array(profile.editor(img).getSelection().getSelection())
 
     profile.fillSelection()
     realYield(20)
 
-    w, h = xcanvas.GetClientSize().Get()
+    w, h = canvas.GetClientSize().Get()
 
     voxels = np.zeros((10, 3), dtype=np.int32)
-    voxels[:, 0] = 8
-    voxels[:, 1] = pxs
-    voxels[:, 2] = pys
+    voxels[:, xidx] = pxs
+    voxels[:, yidx] = pys
+    voxels[:, cidx] = 0
 
     xs, ys, zs = voxels.T
 
@@ -78,7 +96,32 @@ def _test_fillSelection(ortho, overlayList, displayCtx):
     assert np.all(sel == exp)
     assert np.all(img[:][xs, ys, zs] == 999)
 
+    overlayList[:] = []
+
 
 
 def test_fillSelection():
-    run_with_orthopanel(_test_fillSelection)
+    img = Image(op.join(datadir, '3d'))
+    tfs = ft.partial(_test_fillSelection, img=img, canvas=0)
+    run_with_orthopanel(tfs)
+    tfs = ft.partial(_test_fillSelection, img=img, canvas=1)
+    run_with_orthopanel(tfs)
+    tfs = ft.partial(_test_fillSelection, img=img, canvas=2)
+    run_with_orthopanel(tfs)
+
+
+def test_fillSelection_2d():
+    img = Image(np.random.randint(1, 65536, (60, 60, 1)))
+    tfs = ft.partial(_test_fillSelection, img=img, canvas=2)
+    run_with_orthopanel(tfs)
+    img.save('one')
+
+    img = Image(np.random.randint(1, 65536, (60, 1, 60)))
+    tfs = ft.partial(_test_fillSelection, img=img, canvas=1)
+    run_with_orthopanel(tfs)
+    img.save('two')
+
+    img = Image(np.random.randint(1, 65536, (1, 60, 60)))
+    tfs = ft.partial(_test_fillSelection, img=img, canvas=0)
+    run_with_orthopanel(tfs)
+    img.save('three')
