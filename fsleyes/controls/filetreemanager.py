cab4c765d594c7fd6bf58600b464e0258cce56b0
Paul McCarthy <pauldmccarthy@gmail.com>
2019-08-08 18:03:40 +0100

    RF: Move all file tree grid variable/filetype/filename management stuff into a separate module, and document it.

---
+++ fsleyes/controls/filetreemanager.py
@@ -0,0 +1,407 @@
+#!/usr/bin/env python
+#
+# filetreemanager.py - Functions and classes used by the FileTreePanel.
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+"""This module contains functions and classes which are used by the
+:class:`.FileTreePanel`.
+
+
+Overview
+--------
+
+
+The :class:`.FileTreePanel` allows the user to navigate structured
+directories, where the files and sub-directories are named according to a
+:mod:`.filetree` specification. The ``FileTreePanel`` allows the user to
+select which file types to display, and to restrict or re-order the files
+with file tree variables.
+
+
+By default, the :class:`.FileTreePanel` will display a list containing one row
+for every combination of variable values; each row will contain all files for
+(the selected file types) which correspond to that combination of variables.
+The user may also choose to display ``<all>`` values of a specific variable on
+a single row.
+
+
+This module handles the mechanics of generating lists of variables and files
+according to the user's settings.
+
+
+In this module, and in the :mod:`.filetreepanel` module, variables which take
+on a different value for each row are referred to as *varying*, and variables
+for which all possible values are displaed on one row are referred to as
+*fixed*.
+
+
+Example
+-------
+
+
+For example, imagine that we have a data set with data for multiple subjects
+(``sub``) and sessions (``ses``), described by this file tree::
+
+    subj-{participant}
+      [ses-{session}]
+        T1w.nii.gz (T1w)
+        {hemi}.gii (surface)
+
+So for one subject and one session, we might have the following files::
+
+    T1.nii.gz
+    L.gii
+    R.gii
+
+So we have two files types (``T1`` and ``surface``), and three variables
+(``sub``, ``ses``, and ``hemi``).  By default, all variables are *varying*, so
+the ``FileTreePanel`` will display this data set like so (the ``x`` indicates
+whether or not each file is present):
+
+
+======= ======= ========= ======  ===========
+``sub`` ``ses`` ``hemi``  ``T1``  ``surface``
+------- ------- --------- ------  -----------
+``1``   ``1``   ``L``     ``x``   ``x``
+``1``   ``1``   ``R``     ``x``   ``x``
+``1``   ``2``   ``L``     ``x``   ``x``
+``1``   ``2``   ``R``     ``x``   ``x``
+``2``   ``1``   ``L``     ``x``   ``x``
+``2``   ``1``   ``R``     ``x``   ``x``
+``2``   ``2``   ``L``     ``x``   ``x``
+``2``   ``2``   ``R``     ``x``   ``x``
+======= ======= ========= ======  ===========
+
+
+However, it may make more sense to display all of the surface files together.
+The user can do this by setting the ``hemi`` variable to ``<all>``, thus
+changing it to a *fixed* variable. This will cause the :class:`.FileTreePanel`
+to re-arrange the grid like so:
+
+
+======= ======= ====== =================== ===================
+``sub`` ``ses`` ``T1`` ``surface[hemi=L]`` ``surface[hemi=R]``
+------- ------- ------ ------------------- -------------------
+``1``   ``1``   ``x``  ``x``               ``x``
+``1``   ``2``   ``x``  ``x``               ``x``
+``2``   ``1``   ``x``  ``x``               ``x``
+``2``   ``2``   ``x``  ``x``               ``x``
+======= ======= ====== =================== ===================
+"""
+
+
+import itertools as it
+import              collections
+
+
+class FileGroup(object):
+    """A ``FileGroup`` represents a single row in the file tree panel list.  It
+    encapsulates a set of values for all varying variables, and a set of files
+    and their associated fixed variable values. These are all accessible as
+    attributes called ``varyings``, ``files``, and ``fixedvars``.
+    """
+
+
+    def __init__(self, varyings, fixed, ftypes, files):
+        """Create a ``FileGroup``.
+
+        :arg varyings: Dict of ``{ var : val }`` mappings containing the
+                       varying variable values.
+
+        :arg fixed:    List containing ``{ var : val }`` mappings, each
+                       containing the fixed variable values for each file.
+
+        :arg ftypes:   List containing the file type for each file.
+
+        :arg files:    List of file names, the same length as ``fixedvars``.
+                       Missing files are represented as ``None``.
+        """
+
+        self.varyings = varyings
+        self.fixed    = fixed
+        self.ftypes   = ftypes
+        self.files    = files
+
+
+def prepareVaryings(query, ftypes, varyings):
+    """Called by :meth:`FileTreeManager.update`. Prepares a dictionary which
+    contains all possible values for each varying variable.
+
+    :arg query:    :class:`.FileTreeQuery` object
+
+    :arg ftypes:   List of file types to be displayed.
+
+    :arg varyings: Dict of ``{ var : value }`` mappings. A value of ``'*'``
+                   indicates that all possible values for this variable
+                   should be used.
+
+    :returns:      A dict of ``{ var : [value] }`` mappings, containing
+                   every possible value for each varying variable.
+    """
+
+    allvars = query.variables()
+
+    # Force a constsient ordering
+    # of the varying variables
+    _varyings = collections.OrderedDict()
+    for var in sorted(varyings.keys()):
+        _varyings[var] = varyings[var]
+    varyings = _varyings
+
+    # Expand the varying dict so that
+    # it contains { var : [value] }
+    # mappings - '*' is replaced with
+    # a list of all possible values,
+    # and a scalar value is replaced
+    # with a list containing just that
+    # value.
+    for var, val in list(varyings.items()):
+
+        # This variable is not relevant for
+        # any of the specified file types.
+        if not any([var in query.variables(ft) for ft in ftypes]):
+            varyings.pop(var)
+            continue
+
+        elif val == '*': varyings[var] = allvars[var]
+        else:            varyings[var] = [val]
+
+    return varyings
+
+
+def prepareFixed(query, ftypes, fixed):
+    """Called by :meth:`.FileTreeManager.update`. Prepares a dictionary
+    which contains all possible values for each fixed variable, and for
+    each file type.
+
+    :arg query:  :class:`.FileTreeQuery` object
+
+    :arg ftypes: List of file types to be displayed
+
+    :arg fixed:  List of fixed variables
+
+    :returns:    A dict of ``{ ftype : { var : [value] } }`` mappings
+                 which, for each file type, contains a dictionary of
+                 all fixed variables and their possible values.
+    """
+
+    allvars = query.variables()
+
+    # Create a dict for each file type
+    # containing { var : [value] }
+    # mappings for all fixed variables
+    # which are relevant to that file
+    # type.
+    _fixed = {}
+    for ftype in ftypes:
+        ftvars        = query.variables(ftype)
+        _fixed[ftype] = {}
+        for var in fixed:
+            if var in ftvars:
+                _fixed[ftype][var] = allvars[var]
+    fixed = _fixed
+
+    return fixed
+
+
+def genColumns(ftypes, varyings, fixed):
+    """Determines all columns which need to be present in a file tree grid
+    for the given file types, varying and fixed variables.
+
+    :arg ftypes:   List of file types to be displayed
+
+    :arg varyings: Dict of ``{ var : [value} }`` mappings, containing all
+                   varying variables and their possible values (see
+                   :func:`prepareVaryings`).
+
+    :arg fixed:    Dict of ``{ ftype : { var : [value] } }`` mappings
+                   which, for each file type, contains a dictionary of
+                   all fixed variables and their possible values.
+
+    :returns:      Two lists which, combined, represent all columns to be
+                   displayed in the file tree grid:
+                    - A list of varying variable names
+                    - A list of tuples, with each tuple containing:
+                      - A file type
+                      - A dict of ``{var : value}`` mappings, containing
+                        fixed variable values
+    """
+
+    varcols   = [var for var, vals in varyings.items() if len(vals) > 1]
+    fixedcols = []
+
+    for ftype in ftypes:
+
+        ftvars    = fixed[ftype]
+        ftvarprod = list(it.product(*[vals for vals in ftvars.values()]))
+
+        for ftvals in ftvarprod:
+            ftvals = {var : val for var, val in zip(ftvars, ftvals)}
+            fixedcols.append((ftype, ftvals))
+
+    return varcols, fixedcols
+
+
+def genFileGroups(query, varyings, fixed):
+    """Generates a list of :class:`FileGroup` objects, each representing one
+    row in a grid defined by the given set of varying and fixed variables.
+
+    :arg query:    :class:`.FileTreeQuery` object
+
+    :arg varyings: Dict of ``{ var : [value} }`` mappings, containing all
+                   varying variables and their possible values (see
+                   :func:`prepareVaryings`).
+
+    :arg fixed:    Dict of ``{ ftype : { var : [value] } }`` mappings
+                   which, for each file type, contains a dictionary of
+                   all fixed variables and their possible values.
+
+    :returns:      A list of ``FileGroup`` objects.
+    """
+
+    # Build a list of file groups - each
+    # file group represents a group of
+    # files to be displayed together,
+    # corresponding to a combination of
+    # varying values
+    filegroups = []
+
+    # loop through all possible
+    # combinations of varying values
+    for rowi, vals in enumerate(it.product(*varyings.values())):
+
+        groupVars   = {var : val for var, val in zip(varyings.keys(), vals)}
+        groupFtypes = []
+        groupFixed  = []
+        groupFiles  = []
+
+        for ftype, ftvars in fixed:
+
+            fname = query.query(ftype, **groupVars, **ftvars)
+
+            # There should only be one file for
+            # each combination of varying+fixed
+            # values
+            if len(fname) == 1: fname = fname[0].filename
+            else:               fname = None
+
+            groupFtypes.append(ftype)
+            groupFixed .append(ftvars)
+            groupFiles .append(fname)
+
+        # Drop rows which have no files
+        if not all([f is None for f in groupFiles]):
+            grp = FileGroup(groupVars, groupFixed, groupFtypes, groupFiles)
+            filegroups.append(grp)
+
+    return filegroups
+
+
+class FileTreeManager(object):
+    """The ``FileTreeManager`` class handles the generation and arranging
+    of varying and fixed variables, and file types, according to a
+    specification of *varying* and *fixed* variables.
+    """
+
+
+    def __init__(self, query):
+        """Create a ``FileTreeManager``.
+
+        :arg query: :class:`.FileTreeQuery` instance
+        """
+
+        self.__query      = query
+        self.__ftypes     = None
+        self.__varyings   = None
+        self.__fixed      = None
+        self.__varcols    = None
+        self.__fixedcols  = None
+        self.__filegroups = None
+
+
+    def update(self, ftypes, varyings, fixed):
+        """Update the internal file tree grid information according to the
+        given file types and variables.
+
+        :arg ftypes:   List of file types that are to be displayed
+
+        :arg varyings: Dict of ``{var : value}`` mappings defining the
+                       varying variables.
+
+        :arg fixed:    List of variable names defining the fixed variables.
+        """
+
+        query              = self.query
+        varyings           = prepareVaryings(query, ftypes, varyings)
+        fixed              = prepareFixed(   query, ftypes, fixed)
+        varcols, fixedcols = genColumns(     ftypes, varyings, fixed)
+        filegroups         = genFileGroups(  query, varyings, fixedcols)
+
+        self.__ftypes     = ftypes
+        self.__varyings   = varyings
+        self.__fixed      = fixed
+        self.__varcols    = varcols
+        self.__fixedcols  = fixedcols
+        self.__filegroups = filegroups
+
+
+    @property
+    def query(self):
+        """Returns the :class:`.FileTreeQuery` object used by this
+        ``FileTreeManager``.
+        """
+
+        return self.__query
+
+
+    @property
+    def ftypes(self):
+        """Returns a list of all file types to be displayed. """
+        return self.__ftypes
+
+
+    @property
+    def varyings(self):
+        """Returns a dict of ``{ var : [value] }`` mappings, containing every
+        possible value for each varying variable.
+        """
+        return self.__varyings
+
+
+    @property
+    def fixed(self):
+        """Returns a dict of ``{ ftype : { var : [value] } }`` mappings which,
+        for each file type, contains a dictionary of all fixed variables and
+        their possible values.
+        """
+        return self.__fixed
+
+
+    @property
+    def varcols(self):
+        """Returns a list of varying variable names to be used as columns for
+        the varying variables.
+        """
+        return self.__varcols
+
+
+    @property
+    def fixedcols(self):
+        """Returns a list of tuples, with each tuple containing:
+          - A file type
+          - A dict of ``{var : value}`` mappings, containing
+            fixed variable values
+
+        Each tuple represents a column for a combination of file type and
+        fixed variable values.
+        """
+        return self.__fixedcols
+
+
+    @property
+    def filegroups(self):
+        """Returns a list containing all of the ``FileGroup`` objects. Each
+        ``FileGroup`` represents one row in the file tree grid.
+        """
+        return self.__filegroups
