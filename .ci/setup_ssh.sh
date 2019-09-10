88638606ba6292bdbddfd53acd7105dd4778b6dd
Paul McCarthy <pauldmccarthy@gmail.com>
2019-09-10 09:24:17 +0100

    CI: Work on both mac and linux docker hosts

--- .ci/setup_ssh.sh
+++ .ci/setup_ssh.sh
@@ -22,6 +22,22 @@ set -e
 
 if [[ -f /.dockerenv ]]; then
 
+  # We have to use different host names to connect
+  # to the docker daemon host on mac as opposed
+  # to on linux.
+  #
+  # On linux (assuming the docker job is running
+  # with --net=host), we can connect via
+  # username@localhost.
+  #
+  # On mac, we have to connect via
+  # username@host.docker.internal
+  if [[ "$CI_RUNNER_TAGS" == *"macOS"* ]]; then
+    if [[ "$FSL_HOST" == *"@localhost" ]]; then
+      FSL_HOST=${FSL_HOST/localhost/host.docker.internal}
+    fi
+  fi
+
   apt-get update -y                           || yum -y check-update                     || true;
   apt-get install -y git rsync openssh-client || yum install -y git rsync openssh-client || true;
 
