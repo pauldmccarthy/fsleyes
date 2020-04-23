e4f8c3f2e4aefac8482133437d771df26d44f62b
Paul McCarthy <pauldmccarthy@gmail.com>
2020-04-23 12:26:55 +0100

    BF: glvolume shader changes broke some other globject renderings

--- fsleyes/gl/gl14/glvector_funcs.py
+++ fsleyes/gl/gl14/glvector_funcs.py
@@ -51,7 +51,11 @@ def compileShaders(self, vertShader):
             'clipTexture'      : 1,
             'imageTexture'     : 2,
             'colourTexture'    : 3,
-            'negColourTexture' : 3
+            'negColourTexture' : 3,
+
+            # glvolume frag shader expects a modulate
+            # alpha texture, but it is not used
+            'modulateTexture'  : 1,
         }
 
     else:
@@ -105,6 +109,11 @@ def updateShaderState(self):
         voxValXform = [voxValXform[0, 0], voxValXform[0, 3], 0, 0]
 
         self.shader.setFragParam('voxValXform', voxValXform)
+
+        # settings expected by glvolume
+        # frag shader, but not used
+        self.shader.setFragParam('negCmap',  [-1, 0, 0, 0])
+        self.shader.setFragParam('modulate', [0, 0, -1, 1])
 
     else:
 
