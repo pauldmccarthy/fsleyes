2876a20cc95def427bc1ddf06cccee16d7e3305d
Paul McCarthy <pauldmccarthy@gmail.com>
2020-07-14 17:58:53 +0100

    RF: Clean up code a bit, only show high DPI option when relevant.

--- fsleyes/gl/__init__.py
+++ fsleyes/gl/__init__.py
@@ -938,6 +938,32 @@ class WXGLCanvasTarget(object):
     """
 
 
+    @staticmethod
+    def canToggleHighDPI():
+        """Return ``True`` if high-DPI scaling can be toggled, ``False``
+        otherwise.
+
+        Under GTK, high DPI support is not possible with wxPython < 4.0.7, as
+        ``wx.Window.GetContentScaleFactor`` always returns 1. Under GTK and
+        from wxPython 4.0.7 onwards, GL canvases are scaled automatically.
+
+        Under macOS and wxpython < 4.1.0, high DPI must be explicitly requested
+        for GL canvases via a Cocoa API call. This can be done via the
+        :meth:`EnableHighDPI` method.
+
+        Under macOS and with wxpython >= 4.1.0, GL canvases are scaled
+        automatically.
+        """
+
+        if platform.system() != 'Darwin':
+            return False
+
+        wxver = getattr(wx, '__version__', '1.0.0')
+        wxver = [int(v) for v in wxver.split('.')[:3])
+
+        return wxver < (4, 1, 0)
+
+
     def __init__(self):
         """Create a ``WXGLCanvasTarget``. """
 
@@ -963,26 +989,7 @@ class WXGLCanvasTarget(object):
         self.__freezeSwapBuffers = False
         self.__context           = context
 
-        # Under linux, high DPI support is not
-        # possible with wxPython < 4.0.7, as
-        # GetContentScaleFactor always returns 1.
-
-        # Under macOS and wxpython < 4.1.0,
-        # high DPI must be explicitly requested
-        # for GL canvases. This can be done via
-        # the EnableHighDPI method. Under Linux,
-        # GL canvases are scaled the same as
-        # other windows.
-        #
-        # Under Linux+wxpython>=4.0.7 and
-        # macOS+wxpython>=4.1.0, high DPI
-        # scaling happens for us automatically
-        plat  = platform.system()
-        wxver = getattr(wx, '__version__', '1.0.0')
-        wxver = [int(v) for v in wxver.split('.')[:3])
-        self.__needEnableHighDpi = (plat == 'Darwin') and (wxver < (4, 1, 0))
-
-        if self.__needEnableHighDpi:
+        if WXGLCanvasTarget.canToggleHighDPI():
             self.__dpiscale = 1.0
         else:
             self.__dpiscale = self.GetContentScaleFactor()
@@ -1208,7 +1215,7 @@ class WXGLCanvasTarget(object):
         # We don't necessarily need to
         # enable high-DPI support - see
         # __init__.
-        if self.__needEnableHighDpi:
+        if not WXGLCanvasTarget.canToggleHighDPI():
             return
 
         if not self._setGLContext():
