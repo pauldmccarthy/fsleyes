c4029c24c87e35d26f8edce1a2feb8704bbbe287
Paul McCarthy <pauldmccarthy@gmail.com>
2021-02-15 17:46:42 +0000

    TEST: New tests for -bi and volume lighting

--- tests/overlays/test_overlay_volume_3d.py
+++ tests/overlays/test_overlay_volume_3d.py
@@ -7,7 +7,7 @@
 
 import pytest
 
-from .. import run_cli_tests, roi
+from .. import run_cli_tests, roi, haveGL21
 
 
 pytestmark = pytest.mark.overlayclitest
@@ -34,31 +34,50 @@ cli_tests = """
 -dl 3d   -r 50
 -dl 3d   -r 75
 
--dl 3d   -cp 25 0   0
--dl 3d   -cp 50 0   0
--dl 3d   -cp 75 0   0
+# blendByIntensity
+-dl -rot 30 -30 30 3d     -ll -dr 4000 8000
+-dl -rot 30 -30 30 3d -bi -ll -dr 4000 8000
 
--dl 3d   -cp 50  0   0
--dl 3d   -cp 50  0 -90
--dl 3d   -cp 50 90 -90
-
--dl 3d   -cp 50 0 0 -cp 50  0 -90
--dl 3d   -cp 50 0 0 -cp 50 45 -45
--dl 3d   -cp 50 0 0 -cp 50 45 -45 -cp 50  90 90
--dl 3d   -cp 50 0 0 -cp 50 45 -45 -cp 50 135 45
-
+# smoothing (only available via CLI)
 -dl 3d -s 0
 -dl 3d -s 1
 -dl 3d -s 3
 -dl 3d -s 6
 
--dl 3d -cp 25 0 0  -cp 50 90 45 -cp 50 90 -45 -m intersection
--dl 3d -cp 75 0 0  -cp 50 90 45 -cp 50 90 -45 -m union
--dl 3d -cp 75 0 0  -cp 50 90 45 -cp 50 90 -45 -m complement
+# clip planes
+-dl 3d -cp 25 0   0
+-dl 3d -cp 50 0   0
+-dl 3d -cp 75 0   0
+
+-dl 3d -cp 50  0   0
+-dl 3d -cp 50  0 -90
+-dl 3d -cp 50 90 -90
+
+-dl 3d -cp 50 0 0 -cp 50  0 -90
+-dl 3d -cp 50 0 0 -cp 50 45 -45
+-dl 3d -cp 50 0 0 -cp 50 45 -45 -cp 50  90 90
+-dl 3d -cp 50 0 0 -cp 50 45 -45 -cp 50 135 45
+
+-dl 3d -cp 25 0 0 -cp 50 90 45 -cp 50 90 -45 -m intersection
+-dl 3d -cp 75 0 0 -cp 50 90 45 -cp 50 90 -45 -m union
+-dl 3d -cp 75 0 0 -cp 50 90 45 -cp 50 90 -45 -m complement
+
 
 # 2D images
 -dl -rot -45 -30 0 {{roi('3d.nii.gz', (0, 17, 0, 14, 6,  7))}} {{roi('3d.nii.gz', (0, 17, 6,  7, 0, 14))}} {{roi('3d.nii.gz', (8,  9, 0, 14, 0, 14))}}
 -dl -rot 125 -45 0 {{roi('3d.nii.gz', (0, 17, 0, 14, 6,  7))}} {{roi('3d.nii.gz', (0, 17, 6,  7, 0, 14))}} {{roi('3d.nii.gz', (8,  9, 0, 14, 0, 14))}}
+"""
+
+
+# volume lighting is only available on GL21
+cli_lighting_tests = """
+3d                        -ns 400
+-lp   0 -45 0          3d -ns 400
+-lp   0 -90 0          3d -ns 400
+-lp  60 -90 0          3d -ns 400
+-lp -60 -90 0          3d -ns 400
+-lp   0 -90 0  -ld 0.6 3d -ns 400
+-lp   0 -90 0          3d -ns 400 -in linear
 """
 
 
@@ -70,5 +89,13 @@ def test_overlay_volume_3d():
     run_cli_tests('test_overlay_volume_3d',
                   cli_tests,
                   extras=extras,
+                  scene='3d',
+                  threshold=40)
+
+
+@pytest.mark.skipif('not haveGL21()')
+def test_overlay_volume_3d_lighting():
+    run_cli_tests('test_overlay_volume_3d_lighting',
+                  cli_lighting_tests,
                   scene='3d',
                   threshold=40)
