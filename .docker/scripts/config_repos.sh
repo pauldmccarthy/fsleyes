a3ff9ddbdc69e50503066cdf49502e21c7c535a5
Paul McCarthy <pauldmccarthy@gmail.com>
2021-02-15 11:37:00 +0000

    CI: Unused script

--- .docker/scripts/config_repos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "deb http://archive.ubuntu.com/ubuntu/  $UBUNTU_VERSION           main restricted universe"  > /etc/apt/sources.list
-echo "deb http://security.ubuntu.com/ubuntu/ $UBUNTU_VERSION-security  main restricted universe" >> /etc/apt/sources.list
-
-apt-get update -y
