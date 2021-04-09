0bc4e3918690ed1ebbf59b9fa007cdb5bf09c568
Paul McCarthy <pauldmccarthy@gmail.com>
2021-04-09 12:30:21 +0100

    RF: Minor re-namings/re-arrangings

---
+++ fsleyes/plugins/controls/melodicclassificationpanel/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+"""This package provides the :class:`.MelodicClassificationPanel`, a FSLeyes
+control which allows labels to be assigned to the volumes of a 4D NIFTI image.
+"""
+
+from .melodicclassificationpanel import MelodicClassificationPanel
