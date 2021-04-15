7b95f61cefe4a87f20ec776e8fa79db43212be20
Paul McCarthy <pauldmccarthy@gmail.com>
2021-04-15 19:33:10 +0100

    MNT: suppport 3.9

--- setup.py
+++ setup.py
@@ -241,6 +241,7 @@ def main():
             'License :: OSI Approved :: Apache Software License',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Topic :: Scientific/Engineering :: Visualization'],
 
