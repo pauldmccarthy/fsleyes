5da63d1698b362fbbc15ac2ba7dbefc8e87a0d4f
Paul McCarthy <pauldmccarthy@gmail.com>
2021-12-02 11:01:54 +0000

    MNT: new error from wxpython

--- fsleyes/filtermain.py
+++ fsleyes/filtermain.py
@@ -60,6 +60,7 @@ FILTERS = [
     r'Warning: Expected min height of view:',
     r'Failed to connect to session manager',
     r'is not a valid AllowedFileType',
+    r'failed in wxFreePoolGC(): Wrong GC',
 ]
 
 
