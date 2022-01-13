RF,BF: Rework things so that scene3dCanvas provides xform in a form that means GLObjects can apply it before mvmat. Looks like depth offset was always being applied on the wrong axis? Fix 3D rendering of 2D meshes

fsleyes/gl/gl21/glvolume_funcs.py
@@ -202,17 +202,16 @@ def draw2D(self, zpos, axes, xform=None):
     """
 
     bbox                           = self.canvas.viewport
-    projmat                        = self.canvas.projectionMatrix
-    viewmat                        = self.canvas.viewMatrix
+    mvpmat                         = self.canvas.mvpMatrix
     vertices, voxCoords, texCoords = self.generateVertices2D(
         zpos, axes, bbox=bbox)
 
     # We apply the MVP matrix here rather than in
     # the shader, as we're only drawing 6 vertices.
-    if xform is None: xform = affine.concat(projmat, viewmat)
-    else:             xform = affine.concat(projmat, viewmat, xform)
+    if xform is not None:
+        mvpmat = affine.concat(mvpmat, xform)
 
-    vertices = affine.transform(vertices, xform)
+    vertices = affine.transform(vertices, mvpmat)
 
     self.shader.setAtt('vertex',   vertices)
     self.shader.setAtt('voxCoord', voxCoords)
@@ -248,8 +247,8 @@ def draw3D(self, xform=None):
     # We have to apply it *after* the mv
     # transform for occlusion work.
     if xform is not None:
-        mvmat  = affine.concat(         xform, mvmat)
-        mvpmat = affine.concat(projmat, xform, mvmat)
+        mvmat  = affine.concat(mvmat,  xform)
+        mvpmat = affine.concat(mvpmat, xform)
 
     vertices, _, texCoords = self.generateVertices3D(bbox)
     rayStep , texform      = opts.calculateRayCastSettings(mvmat, projmat)
@@ -290,15 +289,14 @@ def drawAll(self, axes, zposes, xforms):
     """Draws all of the specified slices. """
 
     nslices   = len(zposes)
-    projmat   = self.canvas.projectionMatrix
-    viewmat   = self.canvas.viewMatrix
+    mvpmat    = self.mvpMatrix
     vertices  = np.zeros((nslices * 6, 3), dtype=np.float32)
     voxCoords = np.zeros((nslices * 6, 3), dtype=np.float32)
     texCoords = np.zeros((nslices * 6, 3), dtype=np.float32)
 
     for i, (zpos, xform) in enumerate(zip(zposes, xforms)):
 
-        xform     = affine.concat(projmat, viewmat, xform)
+        xform     = affine.concat(mvpmat, xform)
         v, vc, tc = self.generateVertices2D(zpos, axes)
         vertices[ i * 6: i * 6 + 6, :] = affine.transform(v, xform)
         voxCoords[i * 6: i * 6 + 6, :] = vc

fsleyes/gl/glmesh.py
@@ -547,7 +547,7 @@ class GLMesh(globject.GLObject):
         if mdata is None:
             mdata = vdata
 
-        is2D = np.isclose(bhi[2], blo[2])
+        is2D = np.any(np.isclose(bhi, blo))
 
         if opts.wireframe:
             gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE)

fsleyes/gl/scene3dcanvas.py
@@ -115,6 +115,13 @@ class Scene3DCanvas:
 
 
     @property
+    def displayCtx(self):
+        """Returns a reference to the :class:`.DisplayContext` for this canvas.
+        """
+        return self.__displayCtx
+
+
+    @property
     def lightPos(self):
         """Takes the values of :attr:`.Scene3DOpts.lightPos` and
         :attr:`.Scene3DOpts.lightDistance`, and converts it to a position in
@@ -601,7 +608,9 @@ class Scene3DCanvas:
         offset[1]  = ylen * offset[1] / 2
         offset     = affine.scaleOffsetXform(1, offset)
 
-        # And finally the camera.
+        # And finally the camera. Typically
+        # the Z axis is inferior-superior,
+        # so we want that to be pointing up.
         eye     = list(centre)
         eye[1] += 1
         up      = [0, 0, 1]
@@ -683,7 +692,21 @@ class Scene3DCanvas:
         # drawn above (closer to the screen)
         # than lower ones.
         if opts.occlusion:
-            xform = affine.scaleOffsetXform(1, [0, 0, 0.1])
+
+            # The offset is set as a proportion
+            # of the length of the longest axis
+            # in the display bounding box.
+            bounds = self.displayCtx.bounds
+            lens   = [bounds.xlen, bounds.ylen, bounds.zlen]
+            offset = np.max(lens) * 0.01
+
+            # Define the offset vector in view space,
+            # then rotate it into model space (depth
+            # axis is second - see __genViewMatrix)
+            rot   = affine.invert(self.__viewRotate)
+            vec   = affine.transform([0, 1, 0], rot, vector=True)
+            vec   = affine.normalise(vec) * offset
+            xform = affine.scaleOffsetXform(1, vec)
             xform = np.array(xform, dtype=np.float32, copy=False)
         else:
             xform = None