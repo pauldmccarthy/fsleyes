0983c6c531af61aaebead7342253eaa5935fcada
Paul McCarthy <pauldmccarthy@gmail.com>
2022-08-18 17:50:41 +0100

    TEST: update/regenerate lightbox unit tests

--- fsleyes/tests/test_render_lightbox.py
+++ fsleyes/tests/test_render_lightbox.py
@@ -15,65 +15,92 @@ pytestmark = pytest.mark.clitest
 
 
 cli_tests = """
--zx 0 3d.nii.gz
--zx 1 3d.nii.gz
--zx 2 3d.nii.gz
--zx x 3d.nii.gz
--zx y 3d.nii.gz
--zx z 3d.nii.gz
--zx X 3d.nii.gz
--zx Y 3d.nii.gz
--zx Z 3d.nii.gz
+-zr 0 1 -zx 0 3d.nii.gz
+-zr 0 1 -zx 1 3d.nii.gz
+-zr 0 1 -zx 2 3d.nii.gz
+-zr 0 1 -zx x 3d.nii.gz
+-zr 0 1 -zx y 3d.nii.gz
+-zr 0 1 -zx z 3d.nii.gz
+-zr 0 1 -zx X 3d.nii.gz
+-zr 0 1 -zx Y 3d.nii.gz
+-zr 0 1 -zx Z 3d.nii.gz
 
--cb -ls 20 3d.nii.gz
+-zr 0 1 -cb -ls 20 3d.nii.gz
 
--sg     3d.nii.gz
-    -hs 3d.nii.gz
--sg -hs 3d.nii.gz
+-zr 0 1 -sg     3d.nii.gz
+-zr 0 1     -hs 3d.nii.gz
+-zr 0 1 -sg -hs 3d.nii.gz
 
--ss 2 3d.nii.gz
--ss 4 3d.nii.gz
--ss 6 3d.nii.gz
--ss 8 3d.nii.gz
+# 3d.nii.gz has
+#    pixdim 2mm
+#    FOV x=34mm Y=28mm Z=28mm
+# -> slice spacing
+#   x 0.059
+#   y 0.071
+#   z 0.071
 
-# X=34 mm Y=28 mm Z=28 mm
--zx 2        -zr 0 28  3d.nii.gz
--zx 2 -ss 2  -zr 0 28  3d.nii.gz
--zx 2 -ss 4  -zr 0 28  3d.nii.gz
--zx 2 -ss 6  -zr 0 28  3d.nii.gz
--zx 2 -ss 8  -zr 0 28  3d.nii.gz
+-zx 0 -zr 0    1    -ss 0.059 3d.nii.gz
+-zx 0 -zr 0    1    -ss 0.118 3d.nii.gz
+-zx 0 -zr 0    1    -ss 0.176 3d.nii.gz
+-zx 0 -zr 0    1    -ss 0.235 3d.nii.gz
+-zx 0 -zr 0    0.5  -ss 0.059 3d.nii.gz
+-zx 0 -zr 0    0.5  -ss 0.118 3d.nii.gz
+-zx 0 -zr 0    0.5  -ss 0.176 3d.nii.gz
+-zx 0 -zr 0    0.5  -ss 0.235 3d.nii.gz
+-zx 0 -zr 0.25 0.75 -ss 0.059 3d.nii.gz
+-zx 0 -zr 0.25 0.75 -ss 0.118 3d.nii.gz
+-zx 0 -zr 0.25 0.75 -ss 0.176 3d.nii.gz
+-zx 0 -zr 0.25 0.75 -ss 0.235 3d.nii.gz
 
--zx 2        -zr 14 28 3d.nii.gz
--zx 2 -ss 2  -zr 14 28 3d.nii.gz
--zx 2 -ss 4  -zr 14 28 3d.nii.gz
--zx 2 -ss 6  -zr 14 28 3d.nii.gz
--zx 2 -ss 8  -zr 14 28 3d.nii.gz
+-zx 1 -zr 0    1    -ss 0.071 3d.nii.gz
+-zx 1 -zr 0    1    -ss 0.143 3d.nii.gz
+-zx 1 -zr 0    1    -ss 0.214 3d.nii.gz
+-zx 1 -zr 0    1    -ss 0.286 3d.nii.gz
+-zx 1 -zr 0    0.5  -ss 0.071 3d.nii.gz
+-zx 1 -zr 0    0.5  -ss 0.143 3d.nii.gz
+-zx 1 -zr 0    0.5  -ss 0.214 3d.nii.gz
+-zx 1 -zr 0    0.5  -ss 0.286 3d.nii.gz
+-zx 1 -zr 0.25 0.75 -ss 0.071 3d.nii.gz
+-zx 1 -zr 0.25 0.75 -ss 0.143 3d.nii.gz
+-zx 1 -zr 0.25 0.75 -ss 0.214 3d.nii.gz
+-zx 1 -zr 0.25 0.75 -ss 0.286 3d.nii.gz
 
--zx 2        -zr 0 14  3d.nii.gz
--zx 2 -ss 2  -zr 0 14  3d.nii.gz
--zx 2 -ss 4  -zr 0 14  3d.nii.gz
--zx 2 -ss 6  -zr 0 14  3d.nii.gz
--zx 2 -ss 8  -zr 0 14  3d.nii.gz
+-zx 2 -zr 0    1    -ss 0.071 3d.nii.gz
+-zx 2 -zr 0    1    -ss 0.143 3d.nii.gz
+-zx 2 -zr 0    1    -ss 0.214 3d.nii.gz
+-zx 2 -zr 0    1    -ss 0.286 3d.nii.gz
+-zx 2 -zr 0    0.5  -ss 0.071 3d.nii.gz
+-zx 2 -zr 0    0.5  -ss 0.143 3d.nii.gz
+-zx 2 -zr 0    0.5  -ss 0.214 3d.nii.gz
+-zx 2 -zr 0    0.5  -ss 0.286 3d.nii.gz
+-zx 2 -zr 0.25 0.75 -ss 0.071 3d.nii.gz
+-zx 2 -zr 0.25 0.75 -ss 0.143 3d.nii.gz
+-zx 2 -zr 0.25 0.75 -ss 0.214 3d.nii.gz
+-zx 2 -zr 0.25 0.75 -ss 0.286 3d.nii.gz
 
--zx 2        -zr 7 21  3d.nii.gz
--zx 2 -ss 2  -zr 7 21  3d.nii.gz
--zx 2 -ss 4  -zr 7 21  3d.nii.gz
--zx 2 -ss 6  -zr 7 21  3d.nii.gz
--zx 2 -ss 8  -zr 7 21  3d.nii.gz
-
-       -nc  5  3d.nii.gz
--nr  5         3d.nii.gz
--nr 10 -nc  5  3d.nii.gz
--nr  5 -nc 10  3d.nii.gz
--nr 10 -nc 10  3d.nii.gz
+-zr 0 1 -nr 1                 3d.nii.gz
+-zr 0 1 -nr 2                 3d.nii.gz
+-zr 0 1 -nr 3                 3d.nii.gz
+-zr 0 1       -nc 1           3d.nii.gz
+-zr 0 1       -nc 2           3d.nii.gz
+-zr 0 1       -nc 3           3d.nii.gz
+-zr 0 1 -nr 1 -nc 1           3d.nii.gz
+-zr 0 1 -nr 1 -nc 2           3d.nii.gz
+-zr 0 1 -nr 1 -nc 3           3d.nii.gz
+-zr 0 1 -nr 1 -nc 1 -ss 0.143 3d.nii.gz
+-zr 0 1 -nr 1 -nc 2 -ss 0.143 3d.nii.gz
+-zr 0 1 -nr 1 -nc 3 -ss 0.143 3d.nii.gz
 
 # cursor width
--cw 5  3d
--cw 10 3d
+-zr 0 1 -cw 5  3d
+-zr 0 1 -cw 10 3d
 
 # fsl/fsleyes/fsleyes!333
-    3d -cm blue-lightblue {{roi('3d', (0, 6, 0, 14, 0, 14))}}
--no 3d -cm blue-lightblue {{roi('3d', (0, 6, 0, 14, 0, 14))}}
+# L/R flip for neuro-orientation option in lightbox view was being applied
+# around the centre of each overlay's FOV, rather than around the centre of
+# the display coordinate system
+-zr 0 1     3d -cm blue-lightblue {{roi('3d', (0, 6, 0, 14, 0, 14))}}
+-zr 0 1 -no 3d -cm blue-lightblue {{roi('3d', (0, 6, 0, 14, 0, 14))}}
 """
 
 
