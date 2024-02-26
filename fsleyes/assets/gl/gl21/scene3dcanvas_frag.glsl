6edf2ab9e0682e8fae97218d66fd80c5d89ad897
manifest-rules <manifest-rules@git.fmrib.ox.ac.uk>
2024-02-26 19:33:28 +0000

    BF: Fix depth sort shader so that it ignores transparent/killed inputs

--- fsleyes/assets/gl/gl21/scene3dcanvas_frag.glsl
+++ fsleyes/assets/gl/gl21/scene3dcanvas_frag.glsl
@@ -25,6 +25,7 @@ varying vec2 fragTexCoord;
 
 /*
  * Sort both the values and indices arrays according to the values.
+ * Entries with an index value of -1 are ignored.
  * https://stackoverflow.com/a/749206
  */
 void isort(inout float values[ {{noverlays}}],
@@ -34,7 +35,16 @@ void isort(inout float values[ {{noverlays}}],
   float ftmp;
 
   for (int i = {{noverlays}} - 1; i >= 0; i--) {
+
+    if (indices[i] == -1) {
+      continue;
+    }
     for (int j = 0; j < i; j++) {
+
+      if (indices[j] == -1) {
+        continue;
+      }
+
       if (values[i] <= values[j]) {
         ftmp       = values[i];
         values[i]  = values[j];
@@ -54,17 +64,24 @@ void main(void) {
   int   indices[{{noverlays}}];
   vec4  rgbas[  {{noverlays}}];
   float depths[ {{noverlays}}];
-
-  /* Initialise indices */
-  for (int i = 0; i < {{noverlays}}; i++) {
-    indices[i] = i;
-  }
+  float depth;
 
   /* Retrieve RGBA and depth values for every overlay */
   {% for i in range(noverlays) %}
   rgbas[ {{i}}] = texture2D(rgba_{{ i}}, fragTexCoord);
   depths[{{i}}] = texture2D(depth_{{i}}, fragTexCoord).r;
   {% endfor %}
+
+  /*
+   * Initialise indices - entries with alpha == 0
+   * are ignored, and their index is set to -1,
+   * which causes the sort routine to ignore them.
+   */
+  for (int i = 0; i < {{noverlays}}; i++) {
+    if (rgbas[i].a == 0) { indices[i] = -1; }
+    else                 { indices[i] =  i; }
+  }
+
 
   /* Sort by depth */
   isort(depths, indices);
@@ -75,10 +92,25 @@ void main(void) {
    */
   rgba = bgColour;
   for (int i = {{noverlays}} - 1; i >= 0; i--) {
+
+    if (indices[i] == -1) {
+      continue;
+    }
+
     rgba = ((rgbas[indices[i]] *      rgbas[indices[i]].a) +
             (rgba              * (1 - rgbas[indices[i]].a)));
   }
 
+  // Set the depth of this fragment to that of the
+  // first (non-transparent) sorted input depth
+  depth = 1;
+  for (int i = 0; i < {{noverlays}}; i++) {
+    if (indices[i] != -1) {
+      depth = depths[i];
+      break;
+    }
+  }
+
   gl_FragColor = rgba;
-  gl_FragDepth = depths[0];
+  gl_FragDepth = depth;
 }
