79fcfc3c725df70b52dfa307dcf0ac52390116fc
Paul McCarthy <pauldmccarthy@gmail.com>
2024-03-25 11:10:46 +0000

    RF: Allow patch ordering to be controlled

--- fsleyes/patches/__init__.py
+++ fsleyes/patches/__init__.py
@@ -16,7 +16,8 @@ def apply():
     path    = mod.__path__
     name    = mod.__name__
     submods = pkgutil.iter_modules(path, f'{name}.')
+    submods = sorted(m[1] for m in submods)
 
-    for _, subname, _ in  submods:
-        log.debug('Applying patch: %s', subname)
-        importlib.import_module(subname)
+    for submod in  submods:
+        log.debug('Applying patch: %s', submod)
+        importlib.import_module(submod)
