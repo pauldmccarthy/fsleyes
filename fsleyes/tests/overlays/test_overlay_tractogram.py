abc92518d3307de2c2610c1a16f1028852b66fbf
Paul McCarthy <pauldmccarthy@gmail.com>
2025-02-20 14:28:49 +0000

    TEST,CI: Tweak tractogram tests, and re-generate and re-validate benchmarks. Make sure that the GL21 test run doesn't invoke GL33 routines.

--- fsleyes/tests/overlays/test_overlay_tractogram.py
+++ fsleyes/tests/overlays/test_overlay_tractogram.py
@@ -37,57 +37,59 @@ zoomsortho = {
 # and linewidth is adapted in the
 # remaining tests below.
 cli_tests_linewidth = """
-{prefix} {zoom40} tractogram/spirals.trk
-{prefix} {zoom40} tractogram/spirals.trk -lw 3
-{prefix} {zoom40} tractogram/spirals.trk -lw 5
-{prefix} {zoom40} tractogram/spirals.trk -lw 10
+{prefix} {loc} {zoom40} tractogram/spirals.trk
+{prefix} {loc} {zoom40} tractogram/spirals.trk -lw 3
+{prefix} {loc} {zoom40} tractogram/spirals.trk -lw 5
+{prefix} {loc} {zoom40} tractogram/spirals.trk -lw 10
 
-{prefix} {zoom20} tractogram/spirals.trk
-{prefix} {zoom20} tractogram/spirals.trk -lw 3
-{prefix} {zoom20} tractogram/spirals.trk -lw 5
-{prefix} {zoom20} tractogram/spirals.trk -lw 10
+{prefix} {loc} {zoom20} tractogram/spirals.trk
+{prefix} {loc} {zoom20} tractogram/spirals.trk -lw 3
+{prefix} {loc} {zoom20} tractogram/spirals.trk -lw 5
+{prefix} {loc} {zoom20} tractogram/spirals.trk -lw 10
 
-{prefix} {zoom200} tractogram/spirals.trk
-{prefix} {zoom200} tractogram/spirals.trk -lw 3
-{prefix} {zoom200} tractogram/spirals.trk -lw 5
-{prefix} {zoom200} tractogram/spirals.trk -lw 10
+{prefix} {loc} {zoom200} tractogram/spirals.trk
+{prefix} {loc} {zoom200} tractogram/spirals.trk -lw 3
+{prefix} {loc} {zoom200} tractogram/spirals.trk -lw 5
+{prefix} {loc} {zoom200} tractogram/spirals.trk -lw 10
 """
 
 cli_tests = """
 # Colour/clip by data baked into the streamline file
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co vdata           -cm hsv -dr -0.75 0.75
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co vdata           -cm hsv -dr -0.75 0.75 -cr -0.75 0.75
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co sdata           -cm hsv -dr -0.75 0.75
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co sdata           -cm hsv -dr -0.75 0.75 -cr -0.75 0.75
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co vdata -cl sdata -cm hsv -dr -0.75 0.75 -cr -0.75 0.75
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co sdata -cl vdata -cm hsv -dr -0.75 0.75 -cr -0.75 0.75
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth}           -cl vdata -cm hsv                -cr -0.75 0.75
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co vdata           -cm hsv -dr -0.75 0.75
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co vdata           -cm hsv -dr -0.75 0.75 -cr -0.5  0.5  # should be no clipping
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co vdata -cl vdata -cm hsv -dr -0.75 0.75 -cr -0.5  0.5  # should be clipping
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co sdata           -cm hsv -dr -0.75 0.75
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co sdata           -cm hsv -dr -0.75 0.75 -cr -0.5  0.5  # should be no clipping
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co sdata -cl sdata -cm hsv -dr -0.75 0.75 -cr -0.75 0.75 # should be clipping
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co vdata -cl sdata -cm hsv -dr -0.75 0.75 -cr -0.75 0.75
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co sdata -cl vdata -cm hsv -dr -0.75 0.75 -cr -0.5  0.5
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth}           -cl vdata -cm hsv                -cr -0.5  0.5
 
 # neg cmap
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co vdata -cm red-yellow -nc blue-lightblue -un
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co vdata -cm red-yellow -nc blue-lightblue -un -dr 0.25 1
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co vdata -cm red-yellow -nc blue-lightblue -un
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co vdata -cm red-yellow -nc blue-lightblue -un -dr 0.25 1
 
 # code pathways for per-streamline/vertex data are
 # more or less identical, so no need to test both
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co tractogram/spirals_streamline_data.txt
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co tractogram/spirals_vertex_data.txt
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co tractogram/spirals_vertex_data.txt -cm hsv
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co tractogram/spirals_vertex_data.txt -cm hsv -ll -dr 0 25 -cr 25 50
-{prefix} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co tractogram/spirals_vertex_data.txt -cl tractogram/spirals_streamline_data.txt -cm hsv -ll -dr 0 25 -cr 25 50
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co tractogram/spirals_streamline_data.txt
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co tractogram/spirals_vertex_data.txt
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co tractogram/spirals_vertex_data.txt -cm hsv
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co tractogram/spirals_vertex_data.txt -cl tractogram/spirals_streamline_data.txt -cm hsv -ll -dr 0 25 -cr 25 50
+{prefix} {loc} {zoom40}  tractogram/spirals.trk -lw {linewidth} -co tractogram/spirals_vertex_data.txt -cl tractogram/spirals_streamline_data.txt -cm hsv -ll -cr 17 34
 
 # colour/clip by image
-{prefix} {zoom40}  3d -d tractogram/spirals.trk -lw {linewidth} -co 3d
-{prefix} {zoom40}  3d -d tractogram/spirals.trk -lw {linewidth} -co 3d -cm hot
-{prefix} {zoom40}  3d -d tractogram/spirals.trk -lw {linewidth} -cl 3d -cr 5000 8000
-{prefix} {zoom40}  3d -d tractogram/spirals.trk -lw {linewidth} -co 3d -cl vdata -cm hot -dr 4000 8000 -cr -0.75 0.75
+{prefix} {zoom40} 3d {opts3d} -a {alpha3d} tractogram/spirals.trk -ri 3d -lw {linewidth} -co 3d
+{prefix} {zoom40} 3d {opts3d} -a {alpha3d} tractogram/spirals.trk -ri 3d -lw {linewidth} -co 3d -cm hot
+{prefix} {zoom40} 3d {opts3d} -a {alpha3d} tractogram/spirals.trk -ri 3d -lw {linewidth} -cl 3d -cr 5000 8000
+{prefix} {zoom40} 3d {opts3d} -a {alpha3d} tractogram/spirals.trk -ri 3d -lw {linewidth} -co 3d -cl vdata -cm hot -dr 4000 8000 -cr -0.5 0.5
 
 # sub-sampling
-{prefix} {zoom40} {{{{reseed('tractogram/spirals.trk')}}}} -lw {linewidth} -s 75
-{prefix} {zoom40} {{{{reseed('tractogram/spirals.trk')}}}} -lw {linewidth} -s 50
-{prefix} {zoom40} {{{{reseed('tractogram/spirals.trk')}}}} -lw {linewidth} -s 25
-{prefix} {zoom40} {{{{reseed('tractogram/spirals.trk')}}}} -lw {linewidth} -s 75 -co vdata -cm hot
-{prefix} {zoom40} {{{{reseed('tractogram/spirals.trk')}}}} -lw {linewidth} -s 50 -co vdata -cm hot
-{prefix} {zoom40} {{{{reseed('tractogram/spirals.trk')}}}} -lw {linewidth} -s 25 -co vdata -cm hot
+{prefix} {loc} {zoom40} {{{{reseed('tractogram/spirals.trk')}}}} -lw {linewidth} -s 75
+{prefix} {loc} {zoom40} {{{{reseed('tractogram/spirals.trk')}}}} -lw {linewidth} -s 50
+{prefix} {loc} {zoom40} {{{{reseed('tractogram/spirals.trk')}}}} -lw {linewidth} -s 25
+{prefix} {loc} {zoom40} {{{{reseed('tractogram/spirals.trk')}}}} -lw {linewidth} -s 75 -co vdata -cm hot
+{prefix} {loc} {zoom40} {{{{reseed('tractogram/spirals.trk')}}}} -lw {linewidth} -s 50 -co vdata -cm hot
+{prefix} {loc} {zoom40} {{{{reseed('tractogram/spirals.trk')}}}} -lw {linewidth} -s 25 -co vdata -cm hot
 """
 
 cli_refImage_tests = """
@@ -144,10 +146,14 @@ def reseed(f):
     return f
 extras  = {'reseed' : reseed}
 
-@pytest.mark.skipif('not haveGL(2.1)')
+
+@pytest.mark.skipif('(not haveGL(2.1)) or haveGL(3.3)')
 def test_overlay_tractogram_2d_gl21():
-    fmtargs = { 'prefix'    : '-sortho -wl 0 -10 25',
+    fmtargs = { 'prefix'    : '-sortho -hc -hl',
                 'linewidth' : '10',
+                'loc'       : '-wl 0 -10 25',
+                'opts3d'    : '',
+                'alpha3d'   : '50',
                 **zoomsortho}
     tests   = cli_tests_linewidth + cli_tests
     tests   = tests.format(**fmtargs)
@@ -156,18 +162,14 @@ def test_overlay_tractogram_2d_gl21():
                   extras=extras)
 
 
-@pytest.mark.skipif('not haveGL(2.1)')
-def test_overlay_tractogram_2d_refImage():
-    run_cli_tests('test_overlay_tractogram_2d_refImage',
-                  cli_refImage_tests,
-                  extras=extras)
-
-
 @pytest.mark.skipif('not haveGL(3.3)')
 @pytest.mark.gl33test
 def test_overlay_tractogram_2d_gl33():
-    fmtargs = { 'prefix'    : '-sortho -wl 0 -10 25',
+    fmtargs = { 'prefix'    : '-sortho -hc -hl',
                 'linewidth' : '10',
+                'loc'       : '-wl 0 -10 25',
+                'opts3d'    : '',
+                'alpha3d'   : '50',
                 **zoomsortho}
     tests   = cli_tests_linewidth + cli_tests
     tests   = tests.format(**fmtargs)
@@ -176,21 +178,42 @@ def test_overlay_tractogram_2d_gl33():
                   extras=extras)
 
 
-@pytest.mark.skipif('not haveGL(2.1)')
+@pytest.mark.skipif('(not haveGL(2.1)) or haveGL(3.3)')
 def test_overlay_tractogram_3d_gl21():
-    fmtargs = { 'prefix'    : '-s3d',
+    fmtargs = { 'prefix'    : '-s3d -dl',
                 'linewidth' : '10',
+                'loc'       : '-wl 0 -10 25',
+                'opts3d'    : '-in none -cp 50 0 -90',
+                'alpha3d'   : '85',
                 **zooms3d}
     tests   = cli_tests.format(**fmtargs)
     run_cli_tests('test_overlay_tractogram_3d_gl21',
                   tests,
                   extras=extras)
 
-@pytest.mark.skipif('not haveGL(2.1)')
+
+@pytest.mark.skipif('not haveGL(3.3)')
+@pytest.mark.gl33test
+def test_overlay_tractogram_3d_gl33():
+    fmtargs = { 'prefix'    : '-s3d -dl',
+                'linewidth' : '10',
+                'loc'       : '-wl 0 -10 25',
+                'opts3d'    : '-in none -cp 50 0 -90',
+                'alpha3d'   : '85',
+                **zooms3d}
+    tests   = cli_tests_linewidth + cli_tests
+    tests   = tests.format(**fmtargs)
+    run_cli_tests('test_overlay_tractogram_3d_gl33',
+                  tests,
+                  extras=extras)
+
+
+@pytest.mark.skipif('(not haveGL(2.1)) or haveGL(3.3)')
 def test_overlay_tractogram_pseudo3d_gl21():
     run_cli_tests('test_overlay_tractogram_pseudo3d_gl21',
                   cli_pseudo3d_tests,
                   extras=extras)
+
 
 @pytest.mark.skipif('not haveGL(3.3)')
 @pytest.mark.gl33test
@@ -200,16 +223,10 @@ def test_overlay_tractogram_pseudo3d_gl33():
                   extras=extras)
 
 
-@pytest.mark.skipif('not haveGL(3.3)')
-@pytest.mark.gl33test
-def test_overlay_tractogram_3d_gl33():
-    fmtargs = { 'prefix'    : '-s3d',
-                'linewidth' : '5',
-                **zooms3d}
-    tests   = cli_tests_linewidth + cli_tests
-    tests   = tests.format(**fmtargs)
-    run_cli_tests('test_overlay_tractogram_3d_gl33',
-                  tests,
+@pytest.mark.skipif('not haveGL(2.1)')
+def test_overlay_tractogram_2d_refImage():
+    run_cli_tests('test_overlay_tractogram_2d_refImage',
+                  cli_refImage_tests,
                   extras=extras)
 
 
