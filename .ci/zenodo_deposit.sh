7082c20910a2a1543860a71d36ef959f50e1b8dd
Paul McCarthy <pauldmccarthy@gmail.com>
2025-09-10 10:34:02 +0100

    CI: Fix zenodo submission

--- .ci/zenodo_deposit.sh
+++ .ci/zenodo_deposit.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-tmp=`dirname $0`
-pushd $tmp > /dev/null
-thisdir=`pwd`
-popd > /dev/null
+thisdir=$(cd $(dirname $0) && pwd)
+
+pip install .
+pip install requests jinja2
 
 name=$1
 zenodo_url=$2
 zenodo_tkn=$3
 zenodo_depid=$4
 
-version=$(cat fsleyes/version.py | grep "__version__ =" | tr -d " '" | cut -d '=' -f 2)
+version=$(python -c "import fsleyes.version as v; print(v.__version__)")
 upfile=`pwd`/dist/"$name"-"$version".tar.gz
 metafile=`pwd`/.ci/zenodo_meta.json.jinja2
 date=`date +"%Y-%m-%d"`
-
-pip install --retries 10 requests jinja2
 
 python "$thisdir"/zenodo.py \
        "$zenodo_url" \
