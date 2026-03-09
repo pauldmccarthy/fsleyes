9e4c5ab1fd8bf19bc882941b2cadba6b0e84c4f3
Paul McCarthy <pauldmccarthy@gmail.com>
2026-03-09 14:40:10 +0000

    TEST: Move new autoname test alongside other one, make sure it runs within a GUI context in CI

--- fsleyes/tests/test_parseargs.py
+++ fsleyes/tests/test_parseargs.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python
 
 
-import os
 import os.path as op
-import shutil
 
 import numpy as np
 import pytest
 
-from fsl.data.image import Image
-
-from fsl.utils.tempdir import tempdir
-
 from fsleyes.colourmaps import LookupTable
 
-from fsleyes.tests import (run_cli_tests,
-                           discretise,
-                           run_with_fsleyes,
-                           realYield)
+from fsleyes.tests import run_cli_tests, discretise
 
 
 pytestmark = pytest.mark.clitest
@@ -69,42 +60,3 @@ def test_multiple_cmap_lut_files():
     }
 
     run_cli_tests('test_parseargs', tests, extras=extras)
-
-
-
-def test_autoname():
-    run_with_fsleyes(_test_autoname)
-
-def _test_autoname(ortho, overlayList, displayCtx):
-
-    displayCtx.autoNameOverlays = True
-
-    with tempdir():
-
-        os.makedirs('a/b')
-        os.makedirs('a/c', exist_ok=True)
-
-        shutil.copy(f'{datadir}/3d.nii.gz', 'a/b/')
-        shutil.copy(f'{datadir}/3d.nii.gz', 'a/c/')
-
-        i1 = Image('a/b/3d')
-        i2 = Image('a/c/3d')
-
-        overlayList.extend((i1, i2))
-        realYield()
-
-        assert displayCtx.getDisplay(i1).name == 'b/3d'
-        assert displayCtx.getDisplay(i2).name == 'c/3d'
-
-        overlayList[:] = []
-        realYield()
-
-        # fsl/fsleyes/fsleyes!497
-        # don't auto rename  overlays that
-        # have explicitly been given a name
-        i1 = Image('a/b/3d')
-        i2 = Image('a/c/3d')
-        overlayList.extend((i1, i2), name={i1 : 'first', i2 : 'second'})
-        realYield()
-        assert displayCtx.getDisplay(i1).name == 'first'
-        assert displayCtx.getDisplay(i2).name == 'second'
